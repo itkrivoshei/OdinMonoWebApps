e14d3f164303ae68d92769ee7e0e9f33dad76e68
Gitshey <krivoshey.nikita1998@gmail.com>
2020-08-26 21:23:57 +0300

    Add live and over

--- FirstFile.js
+++ FirstFile.js
@@ -1,9 +1,18 @@
 document.getElementById("btn1").onmousedown = function () {onMouseDown()};
 document.getElementById("btn1").onmouseup = function () {onMouseUp()};
 document.getElementById("btn1").onclick = function () {onClickCounter()};
-document.getElementById("btn1").onmouseover = function () {onClickCounter()};
+document.getElementById("btn1").onmouseover = function () {onMouseOver()};
+document.getElementById("btn1").onmouseleave = function () {onMouseLive()};
 
 var i = 1;
+
+function onMouseOver() {
+	document.getElementById("p3").innerHTML = "Mouse Over"
+}
+
+function onMouseLive() {
+	document.getElementById("p3").innerHTML = "Mouse Live"
+}
 
 function onClickCounter() {
 	document.getElementById("p2").innerHTML = `Click count: ${i++}`;
