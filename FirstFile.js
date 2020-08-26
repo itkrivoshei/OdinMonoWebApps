416a22aafbc9a31f4f886e0d56a5a8876d2cdba8
Gitshey <krivoshey.nikita1998@gmail.com>
2020-08-26 16:46:28 +0300

    mouse down mouseup and counter

--- FirstFile.js
+++ FirstFile.js
@@ -1,9 +1,18 @@
-document.getElementById("btn1").addEventListener("click", onMouseDown);
+document.getElementById("btn1").onmousedown = function () {onMouseDown()};
+document.getElementById("btn1").onmouseup = function () {onMouseUp()};
+document.getElementById("btn1").onclick = function () {onClickCounter()};
+document.getElementById("btn1").onmouseover = function () {onClickCounter()};
+
+var i = 1;
+
+function onClickCounter() {
+	document.getElementById("p2").innerHTML = `Click count: ${i++}`;
+}
 
 function onMouseDown() {
-	document.getElementById("p1").innerHTML = "Button DOWN";
+	document.getElementById("p1").innerHTML = "Button: DOWN";
 }
 
 function onMouseUp() {
-	document.getElementById("p1").innerHTML = "Button UP";
-}
+	document.getElementById("p1").innerHTML = "Button: UP";
+} 
