62edb1c4b42f45613ef956d36eb4b18565fe025c
Gitshey <krivoshey.nikita1998@gmail.com>
2020-08-27 20:08:23 +0300

    add api

--- FirstFile.js
+++ FirstFile.js
@@ -3,19 +3,53 @@ document.getElementById("btn1").onmouseup = function () {buttonUp()};
 document.getElementById("btn1").onclick = function () {clickCounter()};
 document.getElementById("btn1").onmouseover = function () {mouseOver()};
 document.getElementById("btn1").onmouseleave = function () {mouseLeave()};
+document.getElementById("btn2").onclick = function () {changeColor()}
 
-var count = 1;
+window.addEventListener("load", ()=> {
+	if(navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(position => {
+			let lon = position.coords.longitude;
+			let lat = position.coords.latitude;
+			let key = "3f3b294c97be06883c4957818ca70c8f";
+			const api = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=curren&appid=${key}`;
+			
+		})
+	}
+});
+
+// Change Color Button
+
+function componentToHex(num) {
+	let hex = num.toString(16);
+	return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+	return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+function changeColor () {
+	let r = Math.floor(Math.random() * 256);
+	let g = Math.floor(Math.random() * 256);
+	let b = Math.floor(Math.random() * 256);
+	hex = rgbToHex(r, g, b);
+	document.getElementById("p4").innerHTML = `Color is: rgb(${r}, ${g}, ${b}) or hex ${hex}`;
+	document.body.style.background = `rgb(${r}, ${g}, ${b})`;
+}
+
+// Info Button
+
+let count = 1;
+function clickCounter() {
+	document.getElementById("p2").innerHTML = `Click count: ${count++}`;
+}
 
 function mouseOver() {
-	document.getElementById("p3").innerHTML = "On Button"
+	document.getElementById("p3").innerHTML = "On Button";
 }
 
 function mouseLeave() {
-	document.getElementById("p3").innerHTML = "Not on Button"
-}
-
-function clickCounter() {
-	document.getElementById("p2").innerHTML = `Click count: ${count++}`;
+	document.getElementById("p3").innerHTML = "Not on Button";
 }
 
 function buttonDown() {
