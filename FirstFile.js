739533774f3f79c7b48a0b3e9a7b75d0e0865ec9
Gitshey <krivoshey.nikita1998@gmail.com>
2020-09-01 21:26:39 +0300

    add scss and format code

--- FirstFile.js
+++ FirstFile.js
@@ -1,10 +1,30 @@
-document.getElementById("btn1").onmousedown = function () {buttonDown()};
-document.getElementById("btn1").onmouseup = function () {buttonUp()};
-document.getElementById("btn1").onclick = function () {clickCounter()};
-document.getElementById("btn1").onmouseover = function () {mouseOver()};
-document.getElementById("btn1").onmouseleave = function () {mouseLeave()};
-document.getElementById("btn2").onclick = function () {changeColor()};
-let temp = document.querySelector(".temp");
+"use strict";
+
+document.getElementById("btn1").onmousedown = function () {
+	buttonDown();
+};
+
+document.getElementById("btn1").onmouseup = function () {
+	buttonUp();
+};
+
+document.getElementById("btn1").onclick = function () {
+	clickCounter();
+};
+
+document.getElementById("btn1").onmouseover = function () {
+	mouseOver();
+};
+
+document.getElementById("btn1").onmouseleave = function () {
+	mouseLeave();
+};
+
+document.getElementById("btn2").onclick = function () {
+	changeColor();
+};
+
+var temp = document.querySelector(".temp");
 
 // window.addEventListener("load", ()=> {
 // 	if(navigator.geolocation) {
@@ -13,7 +33,6 @@ let temp = document.querySelector(".temp");
 // 			let lat = position.coords.latitude;
 // 			let key = config.myKey;
 // 			const api = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=hourly,daily&appid=${key}`;
-
 // 			fetch(api)
 // 				.then(response => {
 // 					return response.json();
@@ -23,14 +42,14 @@ let temp = document.querySelector(".temp");
 // 					const {temperature} = data.currently;
 // 					temp.textContent = temperature
 // 				})
-// 		})	
+// 		})
 // 	}
 // });
 
 // Change Color Button
 
 function componentToHex(num) {
-	let hex = num.toString(16);
+	var hex = num.toString(16);
 	return hex.length == 1 ? "0" + hex : hex;
 }
 
@@ -38,20 +57,28 @@ function rgbToHex(r, g, b) {
 	return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
 
-function changeColor () {
-	let r = Math.floor(Math.random() * 256);
-	let g = Math.floor(Math.random() * 256);
-	let b = Math.floor(Math.random() * 256);
+function changeColor() {
+	var r = Math.floor(Math.random() * 256);
+	var g = Math.floor(Math.random() * 256);
+	var b = Math.floor(Math.random() * 256);
 	hex = rgbToHex(r, g, b);
-	document.getElementById("p4").innerHTML = `Color is: rgb(${r}, ${g}, ${b}) or hex ${hex}`;
-	document.body.style.background = `rgb(${r}, ${g}, ${b})`;
+	document.getElementById("p4").innerHTML = "rgb("
+		.concat(r, ", ")
+		.concat(g, ", ")
+		.concat(b, ") ")
+		.concat(hex);
+	document.body.style.background = "rgb("
+		.concat(r, ", ")
+		.concat(g, ", ")
+		.concat(b, ")");
 }
 
 // Info Button
 
-let count = 1;
+var count = 1;
+
 function clickCounter() {
-	document.getElementById("p2").innerHTML = `Click count: ${count++}`;
+	document.getElementById("p2").innerHTML = "Click count: ".concat(count++);
 }
 
 function mouseOver() {
