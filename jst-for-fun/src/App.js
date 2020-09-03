bdea2505eb3f609b0b78d5e3634945531d4b389b
Joyce Staunton <jstaunto@am-i3.msk.21-school.ru>
2020-09-03 18:13:02 +0300

    Start refactoring, count works

--- jst-for-fun/src/App.js
+++ jst-for-fun/src/App.js
@@ -1,15 +1,54 @@
-import React from "react";
+import React, { useState } from 'react';
+import buttonColor from "./buttonColor";
 
 function App() {
+  // Info Button
 
-  const sayHello = () => {
-    console.log("Heello");
-  };
+  const [count, setCount] = useState(0);
+
+  function mouseOver() {
+    document.getElementById("p3").innerHTML = "On Button";
+  }
+
+  function mouseLeave() {
+    document.getElementById("p3").innerHTML = "Not on Button";
+  }
+
+  function buttonDown() {
+    document.getElementById("p1").innerHTML = "Button: DOWN";
+  }
+
+  function buttonUp() {
+    document.getElementById("p1").innerHTML = "Button: UP";
+  }
+
+  function componentToHex(num) {
+    let hex = num.toString(16);
+    //hex.length === 1 ? "0" + hex : hex;
+  }
+
+  // Change Color Button
+
+  function rgbToHex(r, g, b) {
+    //"#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+  }
+
+  function changeColor() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    let hex = rgbToHex(r, g, b);
+    document.getElementById("p4").innerHTML = `rgb(${r}, ${g}, ${b}) ${hex}`;
+    document.body.style.background = `rgb(${r}, ${g}, ${b})`;
+  }
 
   return (
-    <div>
-      <h1>Hello React</h1>
-      <button onClick={sayHello()}>Hello</button>
+    <div className="buttonInfo">
+      <button onClick={() => setCount(count + 1)}>Down-Up-Click</button>
+      <p>Button UP</p>
+      <p>Click count: {count}</p>
+      <p className="p3">Not on Button</p>
+      <buttonColor />
     </div>
   );
 }
---
+++ jst-for-fun/src/buttonColor.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function buttonColor() {
+    return (
+    <div className="buttonColor">
+      <button classNam="btn2">Change Color</button>
+      <p classNam="p4">rgb(255, 255, 255) #ffffff</p>
+    </div>
+    )
+
+}
+
+export default buttonColor;
