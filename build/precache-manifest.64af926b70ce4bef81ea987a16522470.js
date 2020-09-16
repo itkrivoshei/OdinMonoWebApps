9d16d59c37778778103333bc60187dac4c8ad0ed
Gitshey <krivoshey.nikita1998@gmail.com>
2020-09-16 17:07:08 +0300

    Fix GitPages

---
+++ build/precache-manifest.64af926b70ce4bef81ea987a16522470.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e4cb7093d4f76fcae3841bb415b50c4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ede088f72a8a39f16c9f",
+    "url": "./static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "5192ec7471071d55f3f9",
+    "url": "./static/js/2.778951d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.778951d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ede088f72a8a39f16c9f",
+    "url": "./static/js/main.7d13e3d5.chunk.js"
+  },
+  {
+    "revision": "2d69d9e0d3f71b64c9cb",
+    "url": "./static/js/runtime-main.946e75f8.js"
+  }
+]);
