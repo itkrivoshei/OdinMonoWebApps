2d7b0ca3a276aa8a73aee9637168c708fa12ab69
Gitshey <krivoshey.nikita1998@gmail.com>
2020-10-03 15:26:36 +0300

    Add func Palindrome

--- jst-for-fun/src/App.js
+++ jst-for-fun/src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
 import ColorButton from "./ColorButton";
 import InfoButton from "./InfoButton";
-import "./App.css"
+import "./App.css";
 
 function App() {
+  function palindrome(str) {
+    let orig = str;
+    let rev = str.split("").reverse().join("");
+    if (orig === rev) console.log(true);
+    else console.log(false);
+  }
+
   return (
     <div className="app">
       <InfoButton />
