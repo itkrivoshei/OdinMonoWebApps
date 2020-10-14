794d0cc2e9fe9680dd773253f0e5f0ae251e4a97
Gitshey <krivoshey.nikita1998@gmail.com>
2020-10-14 18:17:15 +0300

    Add Summ Button

---
+++ jst-for-fun/src/SummButton.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function sumNums() {
+  let a = prompt("First Num is:", 0);
+  let b = prompt("Second Num is:", 0);
+  alert(Number(a) + Number(b));
+}
+
+function SummButton() {
+  return(
+    <div className="summButton">
+      <button className="btn4" onClick={() => sumNums()}>Summ two num</button>
+    </div>
+  );
+}
+
+export default SummButton;
