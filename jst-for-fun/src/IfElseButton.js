1d2dda166f4f77f37e887c50e2fb55a64250f646
Joyce Staunton <jstaunto@ga-k5.msk.21-school.ru>
2020-10-18 13:10:32 +0300

    Add IfElseButton file

---
+++ jst-for-fun/src/IfElseButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function IfElseButton() {
+  return (
+    <div className="IfElseButton">
+      <button className="btn"></button>
+    </div>
+  );
+}
+
+export default IfElseButton;
