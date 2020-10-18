dd0334ac45181d6a9d6cc87220aa70cb240e390c
Joyce Staunton <jstaunto@ga-k5.msk.21-school.ru>
2020-10-18 14:59:30 +0300

    Code refactoring and little fix EX4

--- jst-for-fun/src/IfElseButton.js
+++ jst-for-fun/src/IfElseButton.js
@@ -6,56 +6,58 @@ function ifElseTests() {
     // EX1
     if (ex === "1") {
       var ex1 = prompt("Какое «официальное» название JavaScript?");
-      if (ex1 === "ECMAScript"){
+      if (ex1 === "ECMAScript") {
         alert("Верно!");
-      }
-      else
-        alert("Не знаете? ECMAScript!");
+      } else alert("Не знаете? ECMAScript!");
     }
     // EX2
     else if (ex === "2") {
       var ex2 = prompt("Напишите число больше, меньше, либо равное 0", "0");
-      if (ex2[0] == "-" && ex2 < 0)
-        alert(`Число ${ex2} меньше чем 0`);
-      else if (+ex2 > 0)
-        alert(`Число ${+ex2} больше чем 0`);
-      else if (+ex2 === 0)
-        alert("Число ваше число 0");
-      else
-        alert(`${ex2} это не число`);
+      if (ex2[0] == "-" && ex2 < 0) alert(`Число ${ex2} меньше чем 0`);
+      else if (+ex2 > 0) alert(`Число ${+ex2} больше чем 0`);
+      else if (+ex2 === 0) alert("Число ваше число 0");
+      else alert(`${ex2} это не число`);
     }
     // EX3
     else if (ex === "3") {
       var ex3a = prompt("a + b < 100 - Задайте a и b | a =", "0");
       var ex3b = prompt("a + b < 100 - Задайте a и b | b =", "0");
-      var result = ((ex3a && ex3b) == Number) ? (ex3a + ex3b >= 100) ?
-      (ex3a + ex3b == 100) ? "Не верно" : "Не верно"
-      : "Верно"
-      : "Введи только положительные числа";
+      var result =
+        (ex3a && ex3b) == Number
+          ? ex3a + ex3b >= 100
+            ? ex3a + ex3b == 100
+              ? "Не верно"
+              : "Не верно"
+            : "Верно"
+          : "Введи только положительные числа";
       alert(result);
     }
     // EX4
     else if (ex === "4") {
-      var ex4 = prompt("Введите логин: Сотрудник или Директор")
-      var message = ex4 === "Сотрудник" ? "Привет" :
-      ex4 === "Директор" ? "Здравствуйте" :
-      ex4 === "" ? "Нет Логина" : "";
+      var ex4 = prompt("Введите логин: Сотрудник или Директор");
+      var message =
+        ex4 === "Сотрудник"
+          ? "Привет"
+          : ex4 === "Директор"
+          ? "Здравствуйте"
+          : ex4 === ""
+          ? "Нет Логина"
+          : "Не верно, вы директор, или Сотрудник?";
       alert(message);
     }
     // EXIT
-    else if (ex === "5")
-      break;
+    else if (ex === "5") break;
     // ELSE
-    else
-      var ex = prompt("Сделай выбор 1, 2, 3, 4, для выхода 5", "1");
-    }
-
+    else var ex = prompt("Сделай выбор 1, 2, 3, 4, для выхода 5", "1");
+  }
 }
 
 function IfElseButton() {
   return (
     <div className="IfElseButton">
-      <button className="btn" onClick={() => ifElseTests()}>Train If Else</button>
+      <button className="btn" onClick={() => ifElseTests()}>
+        Train If Else
+      </button>
     </div>
   );
 }
