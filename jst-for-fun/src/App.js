960967fe0007904dd6f429b47e3c7f11daac8723
IT-Krivoshey <krivoshey.nikita1998@gmail.com>
2020-10-24 16:27:01 +0300

    Accumulator func as a constructor func

--- jst-for-fun/src/App.js
+++ jst-for-fun/src/App.js
@@ -134,6 +134,15 @@ function App() {
   // alert("Sum = " + calc.sum());
   // alert("Mul = " + calc.mul());
 
+  function Accumulator(startingValue) {
+    this.value = startingValue;
+    this.read = () => (this.value += +prompt("Plus", "0"));
+  }
+  // let accumulator = new Accumulator(1); // начальное значение 1
+  // accumulator.read(); // прибавит ввод prompt к текущему значению
+  // accumulator.read(); // прибавит ввод prompt к текущему значению
+  // alert(accumulator.value); // выведет сумму этих значений
+
   return (
     <div className="app">
       <InfoButton />
