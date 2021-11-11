775ae19db52fe75f2672ed718443bf3ea0e3d449
nikita.krivoshey <krivoshey.nikita1998@gmail.com>
2021-11-11 22:26:12 +0300

    New hint

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -15,4 +15,20 @@ export class AppComponent {
   public showCookie() {
     alert(document.cookie);
   }
+  // Для впуклых углов в css
+  // clip-path: polygon(0 0, 0 100%, 100% 100%, 53% 86%, 24% 73%, 11% 41%);
+
+  // Для фона на половину
+  // .main-header-banner-background:before {
+  //   width: 100%;
+  //   height: 80px;
+  //   background: linear-gradient(258.56deg,#325DE9 31.66%,#548CF6 80.67%);
+  //   content: '';
+  //   position: absolute;
+  //   display: block;
+  // }
+  //
+  // .zindex {
+  //   z-index: 2;
+  // }
 }
