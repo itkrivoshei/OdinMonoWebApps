797f5868ddcbe260d0ac240c45643f85d01831b6
nikita.krivoshey <krivoshey.nikita1998@gmail.com>
2021-11-12 22:47:31 +0300

    Ex

---
+++ gallery-start/main.js
@@ -0,0 +1,14 @@
+var displayedImage = document.querySelector('.displayed-img');
+var thumbBar = document.querySelector('.thumb-bar');
+
+btn = document.querySelector('button');
+var overlay = document.querySelector('.overlay');
+
+/* Looping through images */
+
+  var newImage = document.createElement('img');
+  newImage.setAttribute('src', xxx);
+  thumbBar.appendChild(newImage);
+
+/* Wiring up the Darken/Lighten button */
+
