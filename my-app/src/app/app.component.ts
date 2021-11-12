0cceb82a9b306dff82ce225eb66bbcc6c49d7f5f
nikita.krivoshey <krivoshey.nikita1998@gmail.com>
2021-11-12 22:48:45 +0300

    Delete comments

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -15,26 +15,4 @@ export class AppComponent {
   public showCookie() {
     alert(document.cookie);
   }
-  // Для впуклых углов в css
-  // clip-path: polygon(0 0, 0 100%, 100% 100%, 53% 86%, 24% 73%, 11% 41%);
-
-  // Для фона на половину
-  // .main-header-banner-background:before {
-  //   width: 100%;
-  //   height: 80px;
-  //   background: linear-gradient(258.56deg,#325DE9 31.66%,#548CF6 80.67%);
-  //   content: '';
-  //   position: absolute;
-  //   display: block;
-  // }
-  //
-  // .zindex {
-  //   z-index: 2;
-  // }
-
-  // Для деплоя в Github Pages и Angular
-  // npm i angular-cli-ghpages --save-dev \ Установка
-  // ng build --prod --base-href "https://GithubUserName.github.io/GithubRepoName/" \ Билд
-  // npx angular-cli-ghpages --dir=dist/Project-name \ Деплой
-  //
 }
