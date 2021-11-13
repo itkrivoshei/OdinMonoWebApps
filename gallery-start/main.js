c07fabf1cbce50f2100bc7061453ba2edc737889
nikita.krivoshey <krivoshey.nikita1998@gmail.com>
2021-11-13 17:12:56 +0300

    Create dark selector

--- gallery-start/main.js
+++ gallery-start/main.js
@@ -1,4 +1,9 @@
 let imgBar = document.querySelector('.thumb-bar');
+let dark = document.querySelector('.dark');
+
+dark.onclick = function() {
+
+}
 
 for(let i = 1; i <= 5; i++) {
     imgBar.insertAdjacentHTML('beforeend', `<img src='images/pic${i}.jpg'>`)
