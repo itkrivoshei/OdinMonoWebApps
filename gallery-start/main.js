7ba2054d681fe440cbf9bcf6cdf22c0b63037802
nikita.krivoshey <krivoshey.nikita1998@gmail.com>
2021-11-15 19:10:11 +0300

    Change images

--- gallery-start/main.js
+++ gallery-start/main.js
@@ -6,5 +6,8 @@ dark.onclick = function() {
 }
 
 for(let i = 1; i <= 5; i++) {
-    imgBar.insertAdjacentHTML('beforeend', `<img src='images/pic${i}.jpg'>`)
+    imgBar.insertAdjacentHTML('beforeend', `<img class='img${i}' src='images/pic${i}.jpg'>`)
+    document.querySelector('.img' + i).onclick = function() {
+        document.querySelector('.displayed-img').setAttribute('src', 'images/pic' + i + '.jpg');
+    }
 }
