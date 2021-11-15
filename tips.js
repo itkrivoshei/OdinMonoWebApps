11e495e097fc0bb2d3ecae9c64f978c3c712e2a0
nikita.krivoshey <krivoshey.nikita1998@gmail.com>
2021-11-15 17:29:16 +0300

    Fix tips style

--- tips.js
+++ tips.js
@@ -1,25 +1,28 @@
-// Для впуклых углов в css
-// clip-path: polygon(0 0, 0 100%, 100% 100%, 53% 86%, 24% 73%, 11% 41%);
+/* Для впуклых углов в css
+clip-path: polygon(0 0, 0 100%, 100% 100%, 53% 86%, 24% 73%, 11% 41%);
+*/
 
-// Для фона на половину
-// .main-header-banner-background:before {
-//   width: 100%;
-//   height: 80px;
-//   background: linear-gradient(258.56deg,#325DE9 31.66%,#548CF6 80.67%);
-//   content: '';
-//   position: absolute;
-//   display: block;
-// }
-//
-// .zindex {
-//   z-index: 2;
-// }
+/* Для фона на половину
+.main-header-banner-background:before {
+  width: 100%;
+  height: 80px;
+  background: linear-gradient(258.56deg,#325DE9 31.66%,#548CF6 80.67%);
+  content: '';
+  position: absolute;
+  display: block;
+}
 
-// Для деплоя в Github Pages и Angular
-// npm i angular-cli-ghpages --save-dev \ Установка
-// ng build --prod --base-href "https://GithubUserName.github.io/GithubRepoName/" \ Билд
-// npx angular-cli-ghpages --dir=dist/Project-name \ Деплой
-//
+.zindex {
+  z-index: 2;
+}
+*/
 
-// Для добавления элементов в dom
-// document.querySelector('.какойтСтиль').insertAdjacentHTML('beforeend', '<img src='images/pic1.jpg'>')
+/* Для деплоя в Github Pages и Angular
+ npm i angular-cli-ghpages --save-dev \ Установка
+ ng build --prod --base-href "https://GithubUserName.github.io/GithubRepoName/" \ Билд
+ npx angular-cli-ghpages --dir=dist/Project-name \ Деплой
+*/
+
+/* Для добавления элементов в dom
+document.querySelector('.какойтСтиль').insertAdjacentHTML('beforeend', '<img src='images/pic1.jpg'>')
+*/
