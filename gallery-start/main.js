900f62e6122ee89faa837c6fdc3e883239c2a962
nikita.krivoshey <krivoshey.nikita1998@gmail.com>
2021-11-15 19:30:08 +0300

    Dark img finish gallery

--- gallery-start/main.js
+++ gallery-start/main.js
@@ -1,8 +1,15 @@
 let imgBar = document.querySelector('.thumb-bar');
 let dark = document.querySelector('.dark');
+let btn = document.querySelector('button');
 
 dark.onclick = function() {
-
+    if (this.btn.getAttribute('class') === 'dark') {
+        document.querySelector('.overlay').style.backgroundColor = 'rgba(0,0,0,0.5)';
+        this.btn.setAttribute('class', 'light');
+    } else {
+        document.querySelector('.overlay').style.backgroundColor = 'rgba(0,0,0,0)';
+        this.btn.setAttribute('class', 'dark')
+    }
 }
 
 for(let i = 1; i <= 5; i++) {
