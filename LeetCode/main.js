4f504a74ffdedd317dffc3bb84032baae2f5e89a
Nikita Krivoshei <NikitaKrivoshei@gmail.com>
2022-04-11 20:05:54 +0300

    twoSum

---
+++ LeetCode/main.js
@@ -0,0 +1,11 @@
+function kek(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+};
+
+console.log(kek([3,2,3], 6));
