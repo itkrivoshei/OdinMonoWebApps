4ab14f44a3fab63fca15867dc30750fd4a076f70
Nikita Krivoshei <NikitaKrivoshei@gmail.com>
2022-04-27 17:04:41 +0300

    Update main.js

--- Something/main.js
+++ Something/main.js
@@ -1,11 +1,18 @@
 function sumHell(x = 322, y = 489) {
     let copyX = x.toString();
-    y.toString();
+    let copyY = y.toString();
+    let lastX = copyX.length - 1;
+    let lastY = copyY.length - 1;
+    let res = 0;
+    let bag = 0;
 
     console.log(copyX.length);
 
-    for (let i = 0; i < x.length; i++) {
-
+    for (let i = 0; i < copyX.length; i++) {
+        if (copyX[lastX] + copyY[lastY] > 10) {
+            bag = copyX[lastX];
+            res = copyX[lastX] + copyY[lastY];
+        }
     }
 }
 
