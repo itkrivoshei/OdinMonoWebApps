0175c0e5bdb515ad1f8d9774393dec0784bb4bdd
Nikita Krivoshei <NikitaKrivoshei@gmail.com>
2022-05-06 18:32:05 +0300

    Update main.js

--- LeetCode/main.js
+++ LeetCode/main.js
@@ -70,6 +70,7 @@ Only one valid answer exists.
 Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
 */
 
+
 function twoSum(nums, target) {
     for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
