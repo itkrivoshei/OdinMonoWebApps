702fee6ad10b1e1f7c13bab3e30dcd6ed25418f4
Nikita Krivoshei <NikitaKrivoshei@gmail.com>
2022-05-21 22:21:23 +0300

    Almost finish with time

--- Exs/main.js
+++ Exs/main.js
@@ -1,4 +1,28 @@
-new Date().toLocaleString('default', { weekday: 'long' });
-console.log();
+let timeNow = new Date();
+let hours = timeNow.getHours();
+let idWeekDay = document.getElementById('weekDay');
+let idTime = document.getElementById('time');
 
-let x = '1';
+function takeTimeAmPm() {
+	if (hours > 12) {
+		hours = hours - 12;
+		return hours + ' PM';
+	} else {
+		return hours + ' AM';
+	}
+}
+
+idWeekDay.insertAdjacentHTML(
+	'beforeend',
+	' ' + timeNow.toLocaleString('default', { weekday: 'long' })
+);
+
+idTime.insertAdjacentHTML(
+	'beforeend',
+	' ' +
+		takeTimeAmPm() +
+		' : ' +
+		timeNow.getMinutes() +
+		' : ' +
+		timeNow.getSeconds()
+);
