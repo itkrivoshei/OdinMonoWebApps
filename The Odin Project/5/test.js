1cd7827e6d822aa2932c689f1223c09f2db92b0e
Nikita Krivoshei <NikitaKrivoshei@gmail.com>
2022-07-11 19:44:45 +0300

    Mass and destruction js

--- The Odin Project/5/test.js
+++ The Odin Project/5/test.js
@@ -2,4 +2,26 @@ document.getElementById('first').addEventListener('click', function (e) {
 	console.log(e.target.nodeName);
 });
 
+const mas = [2, 3, 5, 7, 8, '', '22', '0', 1, 21, 43, 0, 54, 21, 122, 1, 56];
+
+//let copy = [];
+// mas.forEach((elem, index, number) => copy.push(elem * 2));
+// let num = mas.reduce((acum, el) => acum + el, 5);
+// const multiply = (el) => el * 3;
+// let num = mas.map(multiply);
+// copy.push(mas.filter((el) => (el > 22 ? true : false)));
+//let max = mas.reduce((acc, el) => (el > acc ? el : acc));
+//mas.forEach((el) => copy.unshift(el));
+//let copy = mas.filter((el) => (!!el ? el : 0));
+//let copy = [...mas.reverse()] + [...mas];
+
+const pers = {
+	name: 'Dima',
+	age: 24,
+};
+	
+const { name, age } = pers;
+
+console.log(name, age);
+
 // slice(start, end) & substring(start, length)
