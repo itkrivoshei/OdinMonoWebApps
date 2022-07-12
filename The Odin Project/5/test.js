8f658ad1ac17ff149cfe627c9c1d07306eed81f2
Nikita Krivoshei <NikitaKrivoshei@gmail.com>
2022-07-12 14:04:47 +0300

    Add as aw + comment

--- The Odin Project/5/test.js
+++ The Odin Project/5/test.js
@@ -2,8 +2,7 @@ document.getElementById('first').addEventListener('click', function (e) {
 	console.log(e.target.nodeName);
 });
 
-const mas = [2, 3, 5, 7, 8, '', '22', '0', 1, 21, 43, 0, 54, 21, 122, 1, 56];
-
+// const mas = [2, 3, 5, 7, 8, '', '22', '0', 1, 21, 43, 0, 54, 21, 122, 1, 56];
 //let copy = [];
 // mas.forEach((elem, index, number) => copy.push(elem * 2));
 // let num = mas.reduce((acum, el) => acum + el, 5);
@@ -15,13 +14,22 @@ const mas = [2, 3, 5, 7, 8, '', '22', '0', 1, 21, 43, 0, 54, 21, 122, 1, 56];
 //let copy = mas.filter((el) => (!!el ? el : 0));
 //let copy = [...mas.reverse()] + [...mas];
 
-const pers = {
-	name: 'Dima',
-	age: 24,
-};
-	
-const { name, age } = pers;
-
-console.log(name, age);
+// const pers = {
+// 	name: 'Dima',
+// 	age: 24,
+// };
+// const { name, age } = pers;
+// console.log(name, age);
 
 // slice(start, end) & substring(start, length)
+
+// fetch('https://jsonplaceholder.typicode.com/posts/1').then((resp) =>
+// 	resp.json().then((json) => console.log(json))
+// );
+
+// async function info() {
+// 	let data = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+// 	let json = await data.json();
+// 	console.log(json);
+// }
+// info();
