323d65786d71cd0529e83ef2d85cdfe283f28e63
Nikita Krivoshei <NikitaKrivoshei@gmail.com>
2022-08-05 20:11:53 +0300

    Finish with Event Loop

--- Some/piscine/5/index.js
+++ Some/piscine/5/index.js
@@ -9,8 +9,24 @@ Promise.resolve(4).then(console.log);
 const asyn = new Promise(() => {
 	console.log(2);
 	setTimeout(() => {
-		console.log('Last')
-	})
+		console.log('Last');
+	});
 });
 
 console.log(3);
+
+console.log('A');
+
+const prom = new Promise((resolve) => {
+	console.log('C');
+
+	setTimeout(() => {
+		console.log('E');
+	});
+
+	resolve(() => console.log('G'));
+});
+
+prom.then(param => param());
+
+console.log('D');
