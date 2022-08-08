4e9e9336b44dc27c2f3d9c0492275a4e80c7d577
Nikita Krivoshei <NikitaKrivoshei@gmail.com>
2022-08-08 19:07:47 +0300

    🧑‍🍳 React

---
+++ Some/piscine/8/test/src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import Counter from './Components/Counter.jsx';
+
+function App() {
+	return (
+		<div className='App'>
+			<Counter />
+		</div>
+	);
+}
+
+export default App;
