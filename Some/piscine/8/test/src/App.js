320cf1b4a3e7a9e9f3aafde4013567ce48edef19
Nikita Krivoshei <NikitaKrivoshei@gmail.com>
2022-08-08 19:23:09 +0300

    Add Imput component

--- Some/piscine/8/test/src/App.js
+++ Some/piscine/8/test/src/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import Counter from './Components/Counter.jsx';
+import Input from './Components/Input.jsx'
 
 function App() {
 	return (
 		<div className='App'>
 			<Counter />
+      <Input />
 		</div>
 	);
 }
