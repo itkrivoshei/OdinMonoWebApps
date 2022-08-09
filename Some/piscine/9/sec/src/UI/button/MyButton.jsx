b6c8ea728719c22ebffc264c7e9cb7cc2944a88f
Nikita Krivoshei <NikitaKrivoshei@gmail.com>
2022-08-09 19:32:40 +0300

    Add button UI

---
+++ Some/piscine/9/sec/src/UI/button/MyButton.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import classes from './MyButton.module.css';
+
+const MyButton = (props) => {
+  return <button className={classes.myBtn}></button>;
+};
+
+export default MyButton;
