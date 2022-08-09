3b12e7b5c3f5a5d1f76616595c68adb5dce77d05
Nikita Krivoshei <NikitaKrivoshei@gmail.com>
2022-08-09 16:03:38 +0300

    Create Post Component

---
+++ Some/piscine/9/sec/src/Components/Post.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Post = () => {
+  return (
+    <div className='post'>
+      <div className='post__text'>
+        <h2>Post title</h2>
+        <p>
+          Ea sunt veniam minim in consectetur adipisicing nisi quis labore
+          cupidatat culpa officia culpa sit.
+        </p>
+      </div>
+      <button>Button</button>
+    </div>
+  );
+};
+
+export default Post;
