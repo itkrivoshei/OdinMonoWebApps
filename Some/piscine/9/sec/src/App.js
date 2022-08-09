02a6b5b8761f6b5ddbed5eb0538176a268df255b
Nikita Krivoshei <NikitaKrivoshei@gmail.com>
2022-08-09 19:24:04 +0300

    Add PostList

--- Some/piscine/9/sec/src/App.js
+++ Some/piscine/9/sec/src/App.js
@@ -1,17 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Counter from './Components/Counter.jsx';
 import Input from './Components/Input.jsx';
 import ClassCounter from './Components/ClassCounter.jsx';
 import './styles/App.css';
 import Post from './Components/Post.jsx';
+import PostsList from './Components/PostsList.jsx';
 
 function App() {
+  const [postsList, setPostsList] = useState([
+    {
+      id: 1,
+      title: 'First title',
+      text: 'Do esse magna irure ex fugiat culpa.',
+    },
+    {
+      id: 2,
+      title: 'Pis',
+      text: 'Do esse magna irure ex fugiat culpa.',
+    },
+    {
+      id: 3,
+      title: 'Lis',
+      text: 'Do esse magna irure ex fugiat culpa.',
+    },
+  ]);
+  const [postsList2, setPostsList2] = useState([
+    {
+      id: 1,
+      title: 'dddddddddddddddddddd',
+      text: 'Do esse magna irure ex fugiat culpa.',
+    },
+    {
+      id: 2,
+      title: 'Pis',
+      text: 'Do esse magna irure ex fugiat culpa.',
+    },
+    {
+      id: 3,
+      title: 'Lis',
+      text: 'Do esse magna irure ex fugiat culpa.',
+    },
+  ]);
+
   return (
     <div className='App'>
       <Counter />
       <Input />
       <ClassCounter />
-      <Post />
+      <PostsList postsList={postsList} title='Post list 1' />
+      <PostsList postsList={postsList2} title='Post list 1' />
     </div>
   );
 }
