0b685715545ec9dcbb31f7a2159bb201da96f41c
Nikita Krivoshei <NikitaKrivoshei@gmail.com>
2022-08-10 15:40:03 +0300

    Move form to component

--- Some/piscine/10/third/src/App.js
+++ Some/piscine/10/third/src/App.js
@@ -5,8 +5,9 @@ import ClassCounter from './Components/ClassCounter.jsx';
 import './styles/App.css';
 // import Post from './Components/Post.jsx';
 import PostsList from './Components/PostsList.jsx';
-import MyButton from './UI/button/MyButton.jsx';
-import MyInput from './UI/input/MyInput.jsx';
+// import MyButton from './UI/button/MyButton.jsx';
+// import MyInput from './UI/input/MyInput.jsx';
+import PostForm from './Components/PostForm.jsx';
 
 function App() {
   const [postsList, setPostsList] = useState([
@@ -26,33 +27,10 @@ function App() {
       text: 'Do esse magna irure ex fugiat culpa.',
     },
   ]);
-  // const [postsList2, setPostsList2] = useState([
-  //   {
-  //     id: 1,
-  //     title: 'dddddddddddddddddddd',
-  //     text: 'Do esse magna irure ex fugiat culpa.',
-  //   },
-  //   {
-  //     id: 2,
-  //     title: 'Pis',
-  //     text: 'Do esse magna irure ex fugiat culpa.',
-  //   },
-  //   {
-  //     id: 3,
-  //     title: 'Lis',
-  //     text: 'Do esse magna irure ex fugiat culpa.',
-  //   },
-  // ]);
 
-  const [post, setPost] = useState({ title: '', text: '' });
-  // const titleInputRef = useRef();
-
-  const addNewPost = (e) => {
-    e.preventDefault();
-    // console.log(titleInputRef.current.value);
-    setPostsList([...postsList, { ...post, id: Date.now() }]);
-    setPost({ title: '', text: '' });
-  };
+  const postUpd = (newPost) => {
+     setPostsList([...postsList, newPost])
+  }
 
   return (
     <div className='App'>
@@ -60,25 +38,7 @@ function App() {
       <Input />
       <ClassCounter />
       <PostsList postsList={postsList} title='Post list 1' />
-      {/* <PostsList postsList={postsList2} title='Post list 1' /> */}
-      <form>
-        {/* Control */}
-        <MyInput
-          onChange={(e) => setPost({ ...post, title: e.target.value })}
-          value={post.title}
-          type='text'
-          placeholder='Header'
-        />
-        {/* No Control */}
-        {/* <MyInput ref={(e) => titleInputRef} type='text' placeholder='Header' /> */}
-        <MyInput
-          onChange={(e) => setPost({ ...post, text: e.target.value })}
-          value={post.text}
-          type='text'
-          placeholder='Text'
-        />
-        <MyButton onClick={addNewPost}>Push</MyButton>
-      </form>
+      <PostForm postCreate={postUpd} />
     </div>
   );
 }
