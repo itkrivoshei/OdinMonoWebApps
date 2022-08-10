f14b0f50506691f19b9e62b7bfaec986e4a68285
Nikita Krivoshei <NikitaKrivoshei@gmail.com>
2022-08-10 11:22:08 +0300

    Update button

--- Some/piscine/10/third/src/UI/button/MyButton.jsx
+++ Some/piscine/10/third/src/UI/button/MyButton.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import classes from './MyButton.module.css';
 
-const MyButton = (props) => {
-  return <button className={classes.myBtn}></button>;
+const MyButton = ({children, ...props}) => {
+  return <button {...props} className={classes.myBtn}>{children}</button>;
 };
 
 export default MyButton;
