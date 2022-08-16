c51daf2946111aa99b2bc1b51ba034510d0e0fbb
Nikita Krivoshei <NikitaKrivoshei@gmail.com>
2022-08-16 20:20:04 +0300

    Map

--- Some/piscine/16/index.js
+++ Some/piscine/16/index.js
@@ -1,3 +1,15 @@
 const arr = [1, 3, 5, 2, 0, 4];
 
-console.log(arr.sort((a, b) => a - b));
+// console.log(arr.sort((a, b) => a - b));
+
+Array.prototype.MyMap = function (fn) {
+  const result = [];
+
+  for (i = 0; i <= this.length; i++) {
+    result.push(fn(this[i], i, this));
+  }
+
+  return result;
+};
+
+console.log(arr.MyMap((el) => (el += 1)));
