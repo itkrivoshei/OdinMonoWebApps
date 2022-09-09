8cace9d9ff42747351c38cbb4638c4abc526cda0
Nikita Krivoshei <NikitaKrivoshei@gmail.com>
2022-09-09 18:04:44 +0300

    📑

--- Some/piscine/12/day-five/src/UI/input/MyInput.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import classes from './MyInput.module.css';
-
-const MyInput = React.forwardRef((props, ref) => {
-  return <input ref={ref} {...props} className={classes.myInput} />;
-});
-
-export default MyInput;
