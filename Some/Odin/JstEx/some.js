c2725541720e9d068cd79964a2e2054089d700fe
Nikita Krivoshei <NikitaKrivoshei@gmail.com>
2022-09-15 13:19:18 +0300

    Guessing game

---
+++ Some/Odin/JstEx/some.js
@@ -0,0 +1,17 @@
+console.log(lastLetter('hilo'));
+
+function add7(num) {
+  return (num += 7);
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function capitalize(text) {
+  return text[0].toUpperCase() + text.slice(1);
+}
+
+function lastLetter(text) {
+  return text[text.length - 1];
+}
