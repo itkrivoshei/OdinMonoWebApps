d7372b5cfe416b98d4b1d28e6f21b723c036b65b
Nikita Krivoshei <NikitaKrivoshei@gmail.com>
2022-10-05 19:05:02 +0300

    Refactoring 🤹

--- Some/Odin/clone01/index.js
+++ Some/Odin/clone01/index.js
@@ -1,29 +1,41 @@
 let htmlBody = document.querySelector('body');
 
-let pRed = document.createElement('p');
+(function createPRedText() {
+  let pRed = document.createElement('p');
 
-pRed.textContent = 'Hey I’m red!';
-pRed.style.color = 'red';
-htmlBody.appendChild(pRed);
+  pRed.textContent = 'Hey I’m red!';
+  pRed.style.color = 'red';
+  htmlBody.appendChild(pRed);
+})();
 
-let h3Red = document.createElement('h3');
+(function createH3BlueText() {
+  let h3Blue = document.createElement('h3');
 
-h3Red.textContent = 'I’m a blue h3!';
-h3Red.style.color = 'blue';
-htmlBody.appendChild(h3Red);
+  h3Blue.textContent = 'I’m a blue h3!';
+  h3Blue.style.color = 'blue';
+  htmlBody.appendChild(h3Blue);
+})();
 
-let divPink = document.createElement('div');
+(function createDivPink() {
+  let divPink = document.createElement('div');
 
-divPink.style.cssText = 'background-color: pink; border: solid black';
+  divPink.style.cssText = 'background-color: pink; border: solid black';
 
-let h1InDiv = document.createElement('h1');
+  (function putInside() {
+    (function createDivPink() {
+      let h1InDiv = document.createElement('h1');
 
-h1InDiv.textContent = 'I’m in a div';
-divPink.appendChild(h1InDiv);
+      h1InDiv.textContent = 'I’m in a div';
+      divPink.appendChild(h1InDiv);
+    })();
 
-let pInDiv = document.createElement('p');
+    (function createDivPink() {
+      let pInDiv = document.createElement('p');
 
-pInDiv.textContent = 'ME TOO!';
-divPink.appendChild(pInDiv);
+      pInDiv.textContent = 'ME TOO!';
+      divPink.appendChild(pInDiv);
+    })();
 
-htmlBody.appendChild(divPink);
+    htmlBody.appendChild(divPink);
+  })();
+})();
