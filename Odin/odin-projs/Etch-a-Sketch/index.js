dfa1d1c9913af5e59e098c2f66c227eb6667d04b
itkrivoshei <nikitakrivoshei@gmail.com>
2022-11-07 01:28:10 +0300

    Base

--- Odin/odin-projs/Etch-a-Sketch/index.js
+++ Odin/odin-projs/Etch-a-Sketch/index.js
@@ -1,7 +1,29 @@
-function drawDivBox(size, divToPut) {
-	
+function drawDivBox(divToPut, size, boxColor, partSize) {
+	let boxi = document.createElement('div');
+	let breakBox = document.createElement('div');
+
+	divToPut.style.display = 'flex';
+	divToPut.style.flexWrap = 'wrap';
+	breakBox.style.flexBasis = '100%';
+	boxi.style.backgroundColor = boxColor;
+	boxi.style.width = partSize + 'px';
+	boxi.style.height = partSize + 'px';
+
+	for (let i = 0; i < size; i++) {
+		for (let i = 0; i < size; i++) {
+			divToPut.appendChild(boxi.cloneNode(true));
+		}
+		divToPut.appendChild(breakBox.cloneNode(true));
+	}
 }
 
-let divWorkSpace = document.querySelector('.work-space');
+function addHowerEffect(parentDiv, color) {
+	parentDiv.addEventListener('mouseover', (e) => {
+		if (e.target.parentNode === parentDiv) {
+			e.target.style.backgroundColor = color;
+		}
+	});
+}
 
-drawDivBox(16, divWorkSpace);
+drawDivBox(document.querySelector('.work-space'), 16, 'yellow', 30);
+addHowerEffect(document.querySelector('.work-space'), 'blue');
