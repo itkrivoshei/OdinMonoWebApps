bfcae649cb8bf5b9f8c6a5ec036ce3ef02f8df64
itkrivoshei <nikitakrivoshei@gmail.com>
2022-12-24 14:10:06 +0300

    Ex 1 done

--- Odin/javascript-exercises/02_repeatString/repeatString.js
+++ Odin/javascript-exercises/02_repeatString/repeatString.js
@@ -1,4 +1,16 @@
-const repeatString = function () {};
+const repeatString = function (word, num) {
+	if (num >= 0) {
+		let res = '';
+
+		for (let i = 0; i < num; i++) {
+			res += word;
+		}
+
+		return res;
+	}
+
+	return 'ERROR'
+};
 
 // Do not edit below this line
 module.exports = repeatString;
