5d8ca123649c04e24139994d8bd65c3aeaefb3e9
itkrivoshei <nikitakrivoshei@gmail.com>
2022-12-24 21:53:53 +0300

    Ex 4 done

--- Odin/javascript-exercises/04_removeFromArray/removeFromArray.js
+++ Odin/javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function () {};
+const removeFromArray = function (arr, ...del) {
+	del.forEach((e) =>
+		arr.splice(
+			arr.findIndex((el) => el === e),
+			typeof arr[arr.findIndex((el) => el === e)] === typeof e ? 1 : 0
+		)
+	);
+
+	return arr;
+};
+
+removeFromArray([1, 2, 3, 4], '1', 3, 2);
 
 // Do not edit below this line
 module.exports = removeFromArray;
