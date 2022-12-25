9c4b1d7b10c06b96863045a1dff232c268ad33f5
itkrivoshei <nikitakrivoshei@gmail.com>
2022-12-25 14:34:34 +0300

    Ex 8 done ✅

--- Odin/javascript-exercises/08_calculator/calculator.js
+++ Odin/javascript-exercises/08_calculator/calculator.js
@@ -1,14 +1,32 @@
-const add = function () {};
+const add = function (x, y) {
+	return x + y;
+};
 
-const subtract = function () {};
+const subtract = function (x, y) {
+	return x - y;
+};
 
-const sum = function () {};
+const sum = function (arr) {
+	return arr[0] ? arr.reduce((acc, num) => (acc += num)) : 0;
+};
 
-const multiply = function () {};
+const multiply = function (arr) {
+	return arr[0] ? arr.reduce((acc, num) => (acc *= num)) : 0;
+};
 
-const power = function () {};
+const power = function (x, y) {
+	return x ** y;
+};
 
-const factorial = function () {};
+const factorial = function (x) {
+	let res = 1;
+
+	for (let i = 1; i <= x; i++) {
+		res *= i;
+	}
+
+	return res;
+};
 
 // Do not edit below this line
 module.exports = {
