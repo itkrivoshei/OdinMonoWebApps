610be6dc5e106d6852dfbdc6a49b895b68357172
itkrivoshei <nikitakrivoshei@gmail.com>
2022-12-25 13:31:33 +0300

    Ex 5 done ✅

--- Odin/javascript-exercises/05_sumAll/sumAll.js
+++ Odin/javascript-exercises/05_sumAll/sumAll.js
@@ -1,4 +1,29 @@
-const sumAll = function () {};
+const sumAll = function (start, end) {
+	if (
+		typeof start !== 'number' ||
+		typeof end !== 'number' ||
+		start < 0 ||
+		end < 0
+	) {
+		return 'ERROR';
+	}
+
+	let res = 0;
+
+	if (start > end) {
+		for (i = end; i <= start; i++) {
+			res += i;
+		}
+
+		return res;
+	}
+
+	for (i = start; i <= end; i++) {
+		res += i;
+	}
+
+	return res;
+};
 
 // Do not edit below this line
 module.exports = sumAll;
