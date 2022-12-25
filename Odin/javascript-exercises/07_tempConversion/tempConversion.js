59febd753d05122e64506daed5084ec4607a9269
itkrivoshei <nikitakrivoshei@gmail.com>
2022-12-25 14:20:41 +0300

    Ex 7 done ✅

--- Odin/javascript-exercises/07_tempConversion/tempConversion.js
+++ Odin/javascript-exercises/07_tempConversion/tempConversion.js
@@ -1,6 +1,10 @@
-const convertToCelsius = function () {};
+const convertToCelsius = function (far) {
+	return Math.round((far - 32) * (5 / 9) * 10) / 10;
+};
 
-const convertToFahrenheit = function () {};
+const convertToFahrenheit = function (cel) {
+	return Math.round((cel * (9 / 5) + 32) * 10) / 10;
+};
 
 // Do not edit below this line
 module.exports = {
