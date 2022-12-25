a6cabdaf1a7745984a0ce3e3f7014d03bee7477a
itkrivoshei <nikitakrivoshei@gmail.com>
2022-12-25 20:44:31 +0300

    Ex 9 done ✅

--- Odin/javascript-exercises/09_palindromes/palindromes.js
+++ Odin/javascript-exercises/09_palindromes/palindromes.js
@@ -1,4 +1,10 @@
-const palindromes = function () {};
+const palindromes = function (str) {
+	const pureStr = str
+		.replace(/[ .,\/#!$%\^&\*;:{}=\-_`~()]/g, '')
+		.toLowerCase();
+
+	return pureStr.split('').reverse().join('') === pureStr ? true : false;
+};
 
 // Do not edit below this line
 module.exports = palindromes;
