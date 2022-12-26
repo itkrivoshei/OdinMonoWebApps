2c7cd9bfb9eb829487327779728192ab8e4f735f
itkrivoshei <nikitakrivoshei@gmail.com>
2022-12-26 13:55:21 +0300

    Ex 10 done ✅

--- Odin/javascript-exercises/10_fibonacci/fibonacci.js
+++ Odin/javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,4 +1,12 @@
-const fibonacci = function () {};
+const fibonacci = function (num, pre = 1, fin = 1, cycle = 1) {
+	if (num < 0) return 'OOPS';
+
+	if (cycle < num) {
+		return fibonacci(num, fin + pre, pre, cycle + 1);
+	}
+
+	return fin;
+};
 
 // Do not edit below this line
 module.exports = fibonacci;
