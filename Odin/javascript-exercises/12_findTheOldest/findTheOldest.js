2faf4b45404a8131a21927fd0866959df9bb7cb4
itkrivoshei <nikitakrivoshei@gmail.com>
2023-01-02 00:15:51 +0300

    Ex 12 done ✅

--- Odin/javascript-exercises/12_findTheOldest/findTheOldest.js
+++ Odin/javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,4 +1,19 @@
-const findTheOldest = function () {};
+const findTheOldest = function (people) {
+	return people.reduce(function (acc, person) {
+		if (!acc[0]) {
+			acc[0] = person;
+			return acc;
+		} else if (
+			(acc[0].yearOfDeath ? acc[0].yearOfDeath : new Date().getFullYear()) -
+				acc[0].yearOfBirth <
+			(person.yearOfDeath ? person.yearOfDeath : new Date().getFullYear()) -
+				person.yearOfBirth
+		) {
+			acc[0] = person;
+		}
+		return acc;
+	}, [])[0];
+};
 
 // Do not edit below this line
 module.exports = findTheOldest;
