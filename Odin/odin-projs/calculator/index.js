7866098f420a6746a427890fc203566c3c4f7cab
itkrivoshei <nikitakrivoshei@gmail.com>
2023-02-13 15:43:16 +0300

    Commit

--- Odin/odin-projs/calculator/index.js
+++ Odin/odin-projs/calculator/index.js
@@ -15,27 +15,32 @@ function divide(x, y) {
 }
 
 function operate(x, y, operator) {
-	if (operator === '+') add(x, y);
-	else if (operator === '-') subtract(x, y);
-	else if (operator === '*') multiply(x, y);
-	else if (operator === '/') divide(x, y);
+	if (operator == '+') return add(x, y);
+	else if (operator === '-') return subtract(x, y);
+	else if (operator === '*') return multiply(x, y);
+	else if (operator === '/') return divide(x, y);
 }
 
 function tapToDisplay(symbol) {
 	const display = document.getElementById('display');
 	if (typeof symbol === 'number') display.innerText += symbol;
-	else if (typeof symbol === 'string' && symbol != 'enter' ) {
-		let firstNum = display.innerText;
-		let operator = symbol;
-
+	else if (typeof symbol === 'string' && symbol != 'enter') {
 		display.innerText += '\n' + symbol + '\n';
-	}
-	else if (symbol === 'enter') {
-		let secondNum = display.innerText.substring(
+	} else if (symbol === 'enter') {
+		let firstNum = +display.innerText.substring(
+			0,
+			display.innerText.indexOf('\n')
+		);
+		let secondNum = +display.innerText.substring(
 			display.innerText.indexOf('\n') + 3,
 			display.innerText.length
 		);
-		console.log(secondNum);
+		let operator = display.innerText.substring(
+			display.innerText.indexOf('\n') + 1,
+			display.innerText.indexOf('\n') + 2
+		);
+
+		display.innerText = operate(firstNum, secondNum, operator);
 	}
 }
 
