acbd6fa15b318b90dc24533cfeb528cd866d55b1
itkrivoshei <nikitakrivoshei@gmail.com>
2023-04-19 19:31:58 +0300

    Ref 🪛

--- React/hacker-stories/src/App.jsx
+++ React/hacker-stories/src/App.jsx
@@ -19,42 +19,34 @@ const list = [
   },
 ];
 
-function App() {
-  return (
-    <div>
-      <h1>Hi</h1>
-      <List />
-      <Search />
-    </div>
-  );
-}
+const App = () => {
+  <div>
+    <h1>Hi</h1>
+    <List />
+    <Search />
+  </div>;
+};
 
-function List() {
-  return (
-    <ul>
-      {list.map(function (item) {
-        return (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span> {item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </li>
-        );
-      })}
-    </ul>
-  );
-}
+const List = () => {
+  <ul>
+    {list.map((item) => {
+      <li key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span> {item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </li>;
+    })}
+  </ul>;
+};
 
-function Search() {
-  return (
-    <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
-    </div>
-  );
-}
+const Search = () => {
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" />
+  </div>;
+};
 
 export default App;
