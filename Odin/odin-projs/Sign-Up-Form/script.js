d993179b910578d6dbcc999f82dd69f1293a8a58
Nikita Krivoshei <NikitaKrivoshei@gmail.com>
2023-05-01 03:13:10 +0300

    Add canvas 🟩

--- Odin/odin-projs/Sign-Up-Form/script.js
+++ Odin/odin-projs/Sign-Up-Form/script.js
@@ -1,4 +1,3 @@
-// script.js
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('registration-form');
   const passwordInput = document.getElementById('password');
@@ -37,3 +36,38 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$%^&*()<>/|';
+const fontSize = 14;
+const columns = canvas.width / fontSize;
+const drops = [];
+
+for (let i = 0; i < columns; i++) {
+  drops[i] = 1;
+}
+
+function drawMatrix() {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = '#0f0';
+  ctx.font = fontSize + "px 'VT323', monospace";
+
+  for (let i = 0; i < drops.length; i++) {
+    const text = characters.charAt(Math.floor(Math.random() * characters.length));
+    ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+
+    if (drops[i] * fontSize > canvas.height || Math.random() > 0.975) {
+      drops[i] = 0;
+    }
+    drops[i]++;
+  }
+}
+
+setInterval(drawMatrix, 40);
