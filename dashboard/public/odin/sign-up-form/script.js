48decf6087547a7f0926ffde147a9490167b3da2
Nikita Krivoshei <NikitaKrivoshei@gmail.com>
2023-05-04 01:23:41 +0300

    🧼

--- dashboard/public/odin/sign-up-form/script.js
+++ dashboard/public/odin/sign-up-form/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const inputs = [
+  const inputIds = [
     'first-name',
     'last-name',
     'email',
@@ -7,15 +7,13 @@ document.addEventListener('DOMContentLoaded', () => {
     'password',
     'confirm-password',
   ];
+
   const form = document.getElementById('registration-form');
 
-  // Phone number mask
   const formatPhoneNumber = (phoneNumber) => {
-    // Strip all non-numeric characters
     const cleaned = ('' + phoneNumber).replace(/\D/g, '').slice(0, 10);
     const match = cleaned.match(/^(\d{0,3})(\d{0,3})(\d{0,4})$/);
 
-    // Format the cleaned phone number
     if (match) {
       return (
         match[1] +
@@ -26,32 +24,32 @@ document.addEventListener('DOMContentLoaded', () => {
     return '';
   };
 
-  // Validation Functions
-  const validatePassword = (password) => {
+  const isPasswordValid = (password) => {
     const regex =
       /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,}$/;
     return regex.test(password);
   };
 
-  const showError = (input, message) => {
-    const errorElement = document.getElementById(`${input}-error`);
+  const showError = (inputId, message) => {
+    const errorElement = document.getElementById(`${inputId}-error`);
     errorElement.textContent = message;
   };
 
-  const validateForm = () => {
+  const isFormValid = () => {
     let valid = true;
 
-    inputs.forEach((input) => {
-      const inputElement = document.getElementById(input);
+    inputIds.forEach((inputId) => {
+      const inputElement = document.getElementById(inputId);
       if (inputElement.value.trim() === '') {
-        showError(input, `${input.replace('-', ' ')} is required.`);
+        showError(inputId, `${inputId.replace('-', ' ')} is required.`);
         valid = false;
       } else {
-        showError(input, '');
+        showError(inputId, '');
       }
     });
 
-    if (!validatePassword(document.getElementById('password').value)) {
+    const passwordElement = document.getElementById('password');
+    if (!isPasswordValid(passwordElement.value)) {
       showError(
         'password',
         'Password must have at least 8 characters, 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character.'
@@ -59,10 +57,8 @@ document.addEventListener('DOMContentLoaded', () => {
       valid = false;
     }
 
-    if (
-      document.getElementById('password').value !==
-      document.getElementById('confirm-password').value
-    ) {
+    const confirmPasswordElement = document.getElementById('confirm-password');
+    if (passwordElement.value !== confirmPasswordElement.value) {
       showError('confirm-password', 'Passwords do not match.');
       valid = false;
     }
@@ -70,114 +66,113 @@ document.addEventListener('DOMContentLoaded', () => {
     return valid;
   };
 
-  const validateInput = (input) => {
-    const inputElement = document.getElementById(input);
+  const isInputValid = (inputId) => {
+    const inputElement = document.getElementById(inputId);
+    const value = inputElement.value.trim();
 
-    // Validate email format
-    if (input === 'email') {
-      const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
-      if (!emailRegex.test(inputElement.value)) {
-        showError(input, 'Please enter a valid email address.');
-        return false;
-      }
-    }
-
-    if (input === 'phone-number') {
-      const phoneRegex = /^\d{3}-\d{3}-\d{4}$/;
-      if (!phoneRegex.test(inputElement.value)) {
-        showError(input, 'Please enter a valid phone number (xxx-xxx-xxxx).');
-        return false;
-      }
-    }
-
-    // Validate password format and confirm password
-    if (input === 'password' || input === 'confirm-password') {
-      if (input === 'password' && !validatePassword(inputElement.value)) {
-        showError(
-          'password',
-          'Password must have at least 8 characters, 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character.'
-        );
-        return false;
-      }
-      if (
-        input === 'confirm-password' &&
-        document.getElementById('password').value !== inputElement.value
-      ) {
-        showError('confirm-password', 'Passwords do not match.');
-        return false;
-      }
-    }
-
-    if (inputElement.value.trim() === '') {
-      showError(input, `${input.replace('-', ' ')} is required.`);
+    if (value === '') {
+      showError(inputId, `${inputId.replace('-', ' ')} is required.`);
       return false;
-    } else {
-      showError(input, '');
-      return true;
     }
+
+    if (inputId === 'email') {
+      const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+      if (!emailRegex.test(value)) {
+        showError(inputId, 'Please enter a valid email address.');
+        return false;
+      }
+    }
+
+    if (inputId === 'phone-number') {
+      const phoneRegex = /^\d{3}-\d{3}-\d{4}$/;
+      if (!phoneRegex.test(value)) {
+        showError(inputId, 'Please enter a valid phone number (xxx-xxx-xxxx).');
+        return false;
+      }
+    }
+
+    if (inputId === 'password' && !isPasswordValid(value)) {
+      showError(
+        'password',
+        'Password must have at least 8 characters, 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character.'
+      );
+      return false;
+    }
+
+    if (
+      inputId === 'confirm-password' &&
+      value !== document.getElementById('password').value
+    ) {
+      showError('confirm-password', 'Passwords do not match.');
+      return false;
+    }
+
+    showError(inputId, '');
+    return true;
   };
 
-  // Event Listeners
   form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    if (validateForm()) {
+    if (isFormValid()) {
       form.submit();
     }
   });
 
-  // Add an input event listener to the phone-number input
   const phoneNumberInput = document.getElementById('phone-number');
   phoneNumberInput.addEventListener('input', (e) => {
     e.target.value = formatPhoneNumber(e.target.value);
   });
 
-  inputs.forEach((input) => {
-    const inputElement = document.getElementById(input);
+  inputIds.forEach((inputId) => {
+    const inputElement = document.getElementById(inputId);
     inputElement.addEventListener('blur', () => {
-      validateInput(input);
+      isInputValid(inputId);
     });
     inputElement.addEventListener('focus', () => {
-      showError(input, '');
+      showError(inputId, '');
     });
   });
 
-  // Matrix Background
-  const canvas = document.getElementById('canvas');
-  const ctx = canvas.getContext('2d');
+  createMatrixBackground();
 
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+  function createMatrixBackground() {
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
 
-  const characters =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$%^&*()<>/|';
-  const fontSize = 14;
-  const columns = canvas.width / fontSize;
-  const drops = [];
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
-  for (let i = 0; i < columns; i++) {
-    drops[i] = 1;
-  }
+    const characters =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$%^&*()<>/|';
+    const fontSize = 14;
+    const columns = canvas.width / fontSize;
+    const drops = [];
 
-  const drawMatrix = () => {
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-    ctx.fillStyle = '#0f0';
-    ctx.font = fontSize + "px 'VT323', monospace";
-
-    for (let i = 0; i < drops.length; i++) {
-      const text = characters.charAt(
-        Math.floor(Math.random() * characters.length)
-      );
-      ctx.fillText(text, i * fontSize, drops[i] * fontSize);
-
-      if (drops[i] * fontSize > canvas.height || Math.random() > 0.975) {
-        drops[i] = 0;
-      }
-      drops[i]++;
+    for (let i = 0; i < columns; i++) {
+      drops[i] = 1;
     }
-  };
 
-  setInterval(drawMatrix, 50);
+    const drawMatrix = () => {
+      ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+      ctx.fillStyle = '#0f0';
+      ctx.font = fontSize + "px 'VT323', monospace";
+
+      for (let i = 0; i < drops.length; i++) {
+        const text = characters.charAt(
+          Math.floor(Math.random() * characters.length)
+        );
+        ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+
+        if (drops[i] * fontSize > canvas.height || Math.random() > 0.975) {
+          drops[i] = 0;
+        }
+        drops[i]++;
+      }
+    };
+
+    setInterval(drawMatrix, 50);
+  }
 });
