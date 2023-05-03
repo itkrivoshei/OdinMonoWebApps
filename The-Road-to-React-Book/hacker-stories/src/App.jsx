006817708bf9eb301be8a6c0505fa37ce79fb9a5
Nikita Krivoshei <NikitaKrivoshei@gmail.com>
2023-05-03 03:19:34 +0300

    Add filter 🪛

--- The-Road-to-React-Book/hacker-stories/src/App.jsx
+++ The-Road-to-React-Book/hacker-stories/src/App.jsx
@@ -1,30 +1,32 @@
-import * as React from "react";
+import * as React from 'react';
 
 const App = () => {
   const stories = [
     {
-      title: "React",
-      url: "https://reactjs.org/",
-      author: "Jordan Walke",
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
       num_comments: 3,
       points: 4,
       objectID: 0,
     },
     {
-      title: "Redux",
-      url: "https://redux.js.org/",
-      author: "Dan Abramov, Andrew Clark",
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
       num_comments: 2,
       points: 5,
       objectID: 1,
     },
   ];
 
-  const [searchTerm, setSearchTerm] = React.useState("");
+  const [searchTerm, setSearchTerm] = React.useState('');
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
+
+  const searchedStories = stories.filter((story) => story.title.toLowerCase().includes(searchTerm.toLowerCase()));
 
   return (
     <div>
@@ -34,7 +36,7 @@ const App = () => {
 
       <hr />
 
-      <List list={stories} />
+      <List list={searchedStories} />
     </div>
   );
 };
