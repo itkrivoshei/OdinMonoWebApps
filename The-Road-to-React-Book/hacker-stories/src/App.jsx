dfe1cfb07b64c4234fd450ef278f7e016563bb12
Nikita Krivoshei <NikitaKrivoshei@gmail.com>
2023-05-05 03:59:33 +0300

    Move back 🔑

--- The-Road-to-React-Book/hacker-stories/src/App.jsx
+++ The-Road-to-React-Book/hacker-stories/src/App.jsx
@@ -54,20 +54,20 @@ const Search = ({ search, onSearch }) => {
 
 const List = ({ list }) => (
   <ul>
-    {list.map(({ objectID, ...item }) => (
-      <Item key={objectID} {...item} />
+    {list.map((item) => (
+      <Item key={item.objectID} item={item} />
     ))}
   </ul>
 );
 
-const Item = ({ title, url, author, num_comments, points }) => (
+const Item = ({ item }) => (
   <li>
     <span>
-      <a href={url}>{title}</a>
+      <a href={item.url}>{item.title}</a>
     </span>
-    <span>{author}</span>
-    <span>{num_comments}</span>
-    <span>{points}</span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
   </li>
 );
 
