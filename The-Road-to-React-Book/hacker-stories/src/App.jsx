edf0179a1de868a29a1e370af3ec6b533f3f60ef
itkrivoshei <nikitakrivoshei@gmail.com>
2023-05-06 15:36:56 +0300

    Add localStorage 🪛

--- The-Road-to-React-Book/hacker-stories/src/App.jsx
+++ The-Road-to-React-Book/hacker-stories/src/App.jsx
@@ -20,10 +20,13 @@ const App = () => {
     },
   ];
 
-  const [searchTerm, setSearchTerm] = React.useState('React');
+  const [searchTerm, setSearchTerm] = React.useState(
+    localStorage.getItem('search') || 'React'
+  );
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
+    localStorage.setItem('search', event.target.value);
   };
 
   const searchedStories = stories.filter((story) =>
