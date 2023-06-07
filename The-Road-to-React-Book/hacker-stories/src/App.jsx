482ffb470da156c46d7ac85cebf71cd2cf6e09e4
itkrivoshei <nikitakrivoshei@gmail.com>
2023-06-07 17:30:18 +0300

    Add useEffect 🪛

--- The-Road-to-React-Book/hacker-stories/src/App.jsx
+++ The-Road-to-React-Book/hacker-stories/src/App.jsx
@@ -24,9 +24,12 @@ const App = () => {
     localStorage.getItem('search') || 'React'
   );
 
+  React.useEffect(() => {
+    localStorage.setItem('search', searchTerm);
+  }, [searchTerm]);
+
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
-    localStorage.setItem('search', event.target.value);
   };
 
   const searchedStories = stories.filter((story) =>
