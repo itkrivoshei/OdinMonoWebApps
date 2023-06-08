f4b294c46f9fa692199628076ec6cfb55da2f166
itkrivoshei <nikitakrivoshei@gmail.com>
2023-06-08 21:03:09 +0300

    Use Fragment 🥸

--- The-Road-to-React-Book/hacker-stories/src/App.jsx
+++ The-Road-to-React-Book/hacker-stories/src/App.jsx
@@ -57,10 +57,10 @@ const App = () => {
 
 const Search = ({ search, onSearch }) => {
   return (
-    <div>
+    <>
       <label htmlFor='search'>Search: </label>
       <input id='search' type='text' value={search} onChange={onSearch} />
-    </div>
+    </>
   );
 };
 
