3165fc04078093235361ab731f5d86215893848f
itkrivoshei <nikitakrivoshei@gmail.com>
2023-08-02 00:09:25 +0300

    Add localStorage to save 🪛

--- dashboard/src/redux/store.js
+++ dashboard/src/redux/store.js
@@ -2,4 +2,37 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
-export default createStore(rootReducer, applyMiddleware(thunk));
+// Function to save state to local storage
+function saveState(state) {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('myState', serializedState);
+  } catch (err) {
+    console.error('Could not save state', err);
+  }
+}
+
+// Function to load state from local storage
+function loadState() {
+  try {
+    const serializedState = localStorage.getItem('myState');
+    if (serializedState === null) return undefined;
+    return JSON.parse(serializedState);
+  } catch (err) {
+    console.error('Could not load state', err);
+    return undefined;
+  }
+}
+
+// Load the persisted state
+const persistedState = loadState();
+
+// Create the Redux store
+const store = createStore(rootReducer, persistedState, applyMiddleware(thunk));
+
+// Subscribe to store changes
+store.subscribe(() => {
+  saveState(store.getState());
+});
+
+export default store;
