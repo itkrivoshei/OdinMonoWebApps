8670a41d7fab9248276ebe143abfe5249c7c1a0f
itkrivoshei <nikitakrivoshei@gmail.com>
2023-08-02 00:08:26 +0300

    Add delete button 🪛

--- dashboard/src/components/toDoApp/TodoItem.js
+++ dashboard/src/components/toDoApp/TodoItem.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { deleteTodo } from '../../redux/actions/todoActions';
 
 function TodoItem({ todo }) {
-  return <li>{todo.text}</li>;
+  const dispatch = useDispatch();
+
+  const handleDelete = () => {
+    dispatch(deleteTodo(todo.id));
+  };
+
+  return (
+    <li>
+      {todo.text}
+      <button onClick={handleDelete}>Delete</button>
+    </li>
+  );
 }
 
 export default TodoItem;
