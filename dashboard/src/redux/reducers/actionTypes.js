051a9588845425685839b1f8b1a048dbc10dfdcd
Nikita Krivoshei <NikitaKrivoshei@gmail.com>
2023-08-13 15:47:25 +0200

    Add actionTypes 😊

---
+++ dashboard/src/redux/reducers/actionTypes.js
@@ -0,0 +1,5 @@
+export const ADD_TODO = 'ADD_TODO';
+export const DELETE_TODO = 'DELETE_TODO';
+export const FETCH_TODOS = 'FETCH_TODOS';
+export const SET_ACTIVE_PROJECT = 'SET_ACTIVE_PROJECT';
+export const ADD_PROJECT = 'ADD_PROJECT';
