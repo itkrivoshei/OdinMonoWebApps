986c67c4700cc8aafcbf7a8ee2a9f46fe2abca41
Nikita Krivoshei <NikitaKrivoshei@gmail.com>
2023-08-13 15:49:32 +0200

    Add projects 🪛

---
+++ dashboard/src/components/toDoApp/AddProject.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addProject } from '../../redux/actions/todoActions';
+
+function AddProject() {
+  const [title, setTitle] = useState('');
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title) {
+      dispatch(addProject(title));
+      setTitle('');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type='text'
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder='Project Title'
+      />
+      <button type='submit'>Add Project</button>
+    </form>
+  );
+}
+
+export default AddProject;
