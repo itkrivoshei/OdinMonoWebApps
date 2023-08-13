c568a8d67a615b4ea1b3119f236bdcc646cc5f3f
Nikita Krivoshei <NikitaKrivoshei@gmail.com>
2023-08-13 15:47:05 +0200

    Upgrade Rducer ⬆️

--- dashboard/src/redux/reducers/todoReducer.js
+++ dashboard/src/redux/reducers/todoReducer.js
@@ -1,14 +1,81 @@
-const initialState = [];
+import {
+  ADD_TODO,
+  DELETE_TODO,
+  FETCH_TODOS,
+  SET_ACTIVE_PROJECT,
+} from './actionTypes';
+
+// Initial state structure for the Redux store.
+export const initialState = {
+  projects: [
+    {
+      id: 'default',
+      title: 'Default Project',
+      todos: [],
+    },
+  ],
+  activeProject: 'default',
+};
 
 const todoReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'ADD_TODO':
-      return [...state, { id: Date.now(), text: action.payload }];
-    case 'DELETE_TODO':
-      return state.filter((todo) => todo.id !== action.payload);
-    case 'FETCH_TODOS':
-      return action.payload;
+    case ADD_TODO:
+      // Map through projects and add todo to the active project's todos array.
+      return {
+        ...state,
+        projects: state.projects.map((project) =>
+          project.id === state.activeProject
+            ? {
+                ...project,
+                todos: [
+                  ...project.todos,
+                  { id: Date.now(), text: action.payload },
+                ],
+              }
+            : project
+        ),
+      };
+
+    case DELETE_TODO:
+      // Map through projects and remove todo from the active project's todos array.
+      return {
+        ...state,
+        projects: state.projects.map((project) =>
+          project.id === state.activeProject
+            ? {
+                ...project,
+                todos: project.todos.filter(
+                  (todo) => todo.id !== action.payload
+                ),
+              }
+            : project
+        ),
+      };
+
+    case FETCH_TODOS:
+      // If fetched todos are valid, create a new project with fetched todos.
+      if (Array.isArray(action.payload)) {
+        return {
+          ...state,
+          projects: [
+            ...state.projects,
+            { id: Date.now(), title: 'Fetched Todos', todos: action.payload },
+          ],
+        };
+      } else {
+        console.error('Invalid FETCH_TODOS payload:', action.payload);
+        return state;
+      }
+
+    case SET_ACTIVE_PROJECT:
+      // Update the active project in the state.
+      return {
+        ...state,
+        activeProject: action.payload,
+      };
+
     default:
+      // Return the existing state if the action type is not handled.
       return state;
   }
 };
