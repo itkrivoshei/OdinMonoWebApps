17cf50da162f88afb1b235f15353d8724db44a63
Nikita Krivoshei <NikitaKrivoshei@gmail.com>
2023-08-13 15:49:45 +0200

    Add Project List 🪛

---
+++ dashboard/src/components/toDoApp/ProjectList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setActiveProject } from '../../redux/actions/todoActions';
+
+function ProjectList() {
+  const projects = useSelector((state) => state.todos.projects) || [];
+  const dispatch = useDispatch();
+
+  const handleProjectClick = (projectId) => {
+    dispatch(setActiveProject(projectId));
+  };
+
+  return (
+    <ul>
+      {projects.map((project) => (
+        <li key={project.id} onClick={() => handleProjectClick(project.id)}>
+          {project.title}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default ProjectList;
