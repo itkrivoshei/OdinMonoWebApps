fb48a4b8746f3c01f27a61278f0c4772182cd1b6
Nikita Krivoshei <NikitaKrivoshei@gmail.com>
2023-08-13 15:50:42 +0200

    Add Projects and Validation for mapStateProps 🪛

--- dashboard/src/components/toDoApp/TodoApp.js
+++ dashboard/src/components/toDoApp/TodoApp.js
@@ -2,22 +2,28 @@ import React from 'react';
 import { connect } from 'react-redux';
 import AddTodo from './AddTodo';
 import TodoList from './TodoList';
+import ProjectList from './ProjectList';
+import AddProject from './AddProject';
 import {
   addTodo,
   deleteTodo,
   fetchTodos,
+  addProject,
 } from '../../redux/actions/todoActions';
 
 class TodoApp extends React.Component {
+  // Fetch todos when component mounts.
   componentDidMount() {
     this.props.fetchTodos();
   }
 
   render() {
-    const { todos, addTodo, deleteTodo } = this.props;
+    const { todos, addTodo, deleteTodo, addProject } = this.props;
 
     return (
       <div>
+        <AddProject addProject={addProject} />
+        <ProjectList />
         <AddTodo addTodo={addTodo} />
         <TodoList todos={todos} deleteTodo={deleteTodo} />
       </div>
@@ -25,14 +31,32 @@ class TodoApp extends React.Component {
   }
 }
 
-const mapStateToProps = (state) => ({
-  todos: state.todos,
-});
+// Map state to props to provide active project's todos to TodoList component.
+const mapStateToProps = (state) => {
+  // Check if projects exist in state and if it's an array.
+  if (state.todos && Array.isArray(state.todos.projects)) {
+    // Find the active project.
+    const activeProject = state.todos.projects.find(
+      (project) => project.id === state.todos.activeProject
+    );
+    // Return active project's todos or an empty array if not found.
+    return {
+      todos: activeProject ? activeProject.todos : [],
+    };
+  } else {
+    // Return an empty array if projects is not defined or not an array.
+    return {
+      todos: [],
+    };
+  }
+};
 
+// Map dispatch actions to props.
 const mapDispatchToProps = {
   addTodo,
   deleteTodo,
   fetchTodos,
+  addProject,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoApp);
