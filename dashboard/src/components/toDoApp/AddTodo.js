b4de82fb48f89eeceaa54deb3a01d206167e41d9
itkrivoshei <nikitakrivoshei@gmail.com>
2023-08-13 21:25:20 +0300

    Prevent empty todo 🪛

--- dashboard/src/components/toDoApp/AddTodo.js
+++ dashboard/src/components/toDoApp/AddTodo.js
@@ -1,12 +1,21 @@
 import React, { useState } from 'react';
 
 function AddTodo({ addTodo }) {
+  // State to manage the input value
   const [input, setInput] = useState('');
 
+  // Handler for form submission
   const handleAddTodo = (event) => {
     event.preventDefault();
-    addTodo(input);
-    setInput('');
+
+    // Trim the input to prevent adding whitespace-only todos
+    const trimmedInput = input.trim();
+
+    // Only dispatch the addTodo action if the trimmed input isn't empty
+    if (trimmedInput) {
+      addTodo(trimmedInput);
+      setInput(''); // Reset input field after adding
+    }
   };
 
   return (
@@ -14,7 +23,9 @@ function AddTodo({ addTodo }) {
       <input
         type='text'
         value={input}
+        // Update the input state on every input change
         onChange={(event) => setInput(event.target.value)}
+        placeholder='Enter todo...' // Added a placeholder for better user guidance
       />
       <button type='submit'>Add Todo</button>
     </form>
