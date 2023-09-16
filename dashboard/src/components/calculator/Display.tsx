fcd946c6a019045e98390c028fe22c6778c317db
itkrivoshei <nikitakrivoshei@gmail.com>
2023-09-16 19:42:20 +0200

    Add Display 🪛

---
+++ dashboard/src/components/calculator/Display.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface DisplayProps {
+  value: string;
+  numLockFlash: boolean;
+}
+
+const Display: React.FC<DisplayProps> = ({ value, numLockFlash }) => {
+  return (
+    <div className={`display ${!numLockFlash ? 'lock' : ''}`}>{value}</div>
+  );
+};
+
+export default Display;
