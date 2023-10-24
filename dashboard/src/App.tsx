3e0aef50481588d0e30a5ad60ca5f562733c44eb
itkrivoshei <nikitakrivoshei@gmail.com>
2023-10-24 20:27:42 +0200

    🪛 Gh-Pages fixes

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Restaurant from './components/RestaurantPages/Restaurant';
 import ToDoApp from './components/ToDoApp/ToDoApp';
