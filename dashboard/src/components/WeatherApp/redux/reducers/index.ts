33390357e60f09a7b4f50980de0662c4e89fe3a2
itkrivoshei <nikitakrivoshei@gmail.com>
2023-11-13 11:21:47 +0100

    🧹 Move redux

--- dashboard/src/components/WeatherApp/redux/reducers/index.ts
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import { weatherReducer } from './weatherReducer';
-
-export default combineReducers({
-  weather: weatherReducer,
-});
--- dashboard/src/components/WeatherApp/redux/store.ts
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware, Store } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers';
-
-const store: Store = createStore(rootReducer, applyMiddleware(thunk));
-
-export default store;
