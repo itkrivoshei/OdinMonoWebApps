2e60d1923f47b04514250968f2a95574a6046cfe
itkrivoshei <nikitakrivoshei@gmail.com>
2023-12-16 15:24:46 +0100

    🪛 Add City changer

--- dashboard/src/components/WeatherApp/SettingsMenu.tsx
+++ dashboard/src/components/WeatherApp/SettingsMenu.tsx
@@ -1,30 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect, ConnectedProps } from 'react-redux';
 import {
   setUnitCelsius,
   setUnitFahrenheit,
+  fetchWeather,
 } from '../../redux/actions/weatherActions';
 import { RootState } from '../../redux/reducers/rootReducer';
 import { Unit } from '../../redux/reducers/weatherReducer';
 
+// Mapping Redux state to component props
 const mapStateToProps = (state: RootState) => ({
   unit: state.weather.unit,
 });
 
+// Mapping Redux dispatch actions to component props
 const mapDispatchToProps = {
   setUnitCelsius,
   setUnitFahrenheit,
+  fetchWeather,
 };
 
 const connector = connect(mapStateToProps, mapDispatchToProps);
 
 type PropsFromRedux = ConnectedProps<typeof connector>;
 
+// SettingsMenu component
 const SettingsMenu: React.FC<PropsFromRedux> = ({
   setUnitCelsius,
   setUnitFahrenheit,
+  fetchWeather,
   unit,
 }) => {
+  const [city, setCity] = useState('');
+
+  // Function to handle unit toggle
   const toggleUnit = () => {
     if (unit === Unit.Celsius) {
       setUnitFahrenheit();
@@ -33,11 +42,30 @@ const SettingsMenu: React.FC<PropsFromRedux> = ({
     }
   };
 
+  // Function to handle city input change
+  const handleCityChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCity(event.target.value);
+  };
+
+  // Function to handle form submission
+  const handleFetchWeather = () => {
+    fetchWeather({ city });
+  };
+
   return (
     <div>
       <button onClick={toggleUnit}>
         Switch to {unit === Unit.Celsius ? 'Fahrenheit' : 'Celsius'}
       </button>
+      <div>
+        <input
+          type='text'
+          value={city}
+          onChange={handleCityChange}
+          placeholder='Enter city'
+        />
+        <button onClick={handleFetchWeather}>Fetch Weather</button>
+      </div>
     </div>
   );
 };
