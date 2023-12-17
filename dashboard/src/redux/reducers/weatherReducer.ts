60d7fd9fbaa06d2d6765c0632846f5e3bd793b9a
itkrivoshei <nikitakrivoshei@gmail.com>
2023-12-17 19:08:55 +0100

    🧹 Refa

--- dashboard/src/redux/reducers/weatherReducer.ts
+++ dashboard/src/redux/reducers/weatherReducer.ts
@@ -4,11 +4,13 @@ import {
   WeatherActionType,
 } from '../actions/weatherActions';
 
+// Enum for unit measurement to ensure type safety and better developer experience
 export enum Unit {
   Celsius = 'C',
   Fahrenheit = 'F',
 }
 
+// Interface for the state shape of the weather data
 export interface WeatherState {
   loading: boolean;
   weatherData: WeatherData | null;
@@ -16,45 +18,49 @@ export interface WeatherState {
   unit: Unit;
 }
 
+// Initial state of the weather data
 const initialState: WeatherState = {
   loading: false,
   weatherData: null,
   error: null,
-  unit: Unit.Celsius,
+  unit: Unit.Celsius, // Default unit set to Celsius
 };
 
+// Weather reducer function handling various weather-related actions
 const weatherReducer = (
-  state = initialState,
+  state: WeatherState = initialState,
   action: WeatherActionTypes
 ): WeatherState => {
   switch (action.type) {
+    // Handles the start of a weather fetch action
     case WeatherActionType.FETCH_WEATHER_START:
-      return { ...state, loading: true };
+      return { ...state, loading: true, error: null }; // Clear any previous errors
 
+    // Handles successful weather data fetching
     case WeatherActionType.FETCH_WEATHER_SUCCESS:
       return { ...state, loading: false, weatherData: action.payload };
 
+    // Handles weather fetch failure
     case WeatherActionType.FETCH_WEATHER_FAILURE:
       return { ...state, loading: false, error: action.payload };
 
+    // Resets any stored errors in the weather state
     case WeatherActionType.RESET_WEATHER_ERROR:
       return { ...state, error: null };
 
+    // Sets the unit to Celsius
     case WeatherActionType.SET_UNIT_CELSIUS:
-      return {
-        ...state,
-        unit: Unit.Celsius,
-      };
+      return { ...state, unit: Unit.Celsius };
 
+    // Sets the unit to Fahrenheit
     case WeatherActionType.SET_UNIT_FAHRENHEIT:
-      return {
-        ...state,
-        unit: Unit.Fahrenheit,
-      };
+      return { ...state, unit: Unit.Fahrenheit };
 
+    // Default case to return the current state for any non-handled actions
     default:
       return state;
   }
 };
 
+// Default export of the weather reducer
 export default weatherReducer;
