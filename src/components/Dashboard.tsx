1c7ca55d38da53c0ac9efcc93d2c29a7d333cec5
itkrivoshei <nikitakrivoshei@gmail.com>
2023-12-19 18:32:06 +0100

    Refactor Dashboard component and update project list

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -8,32 +8,31 @@ import {
   ListItemText,
   CssBaseline,
   ThemeProvider,
-  createTheme,
 } from '@mui/material';
+import { theme } from './theme';
 
-const Dashboard = () => {
-  const theme = createTheme({
-    palette: {
-      mode: 'dark',
-    },
-  });
+interface Project {
+  path: string;
+  name: string;
+}
 
-  const projects = [
-    { path: '/ToDoApp', name: 'To Do' },
-    { path: '/TicTacToe', name: 'Tic Tac Toe' },
-    { path: '/BookLibrary', name: 'Book Library' },
-    { path: '/SignUpForm', name: 'Sign Up Form' },
-    { path: '/Calculator', name: 'Calculator' },
-    { path: '/EtchASketch', name: 'Etch a Sketch' },
-    { path: '/DrumKit', name: 'Drum Kit' },
-    { path: '/RockPaperScissors', name: 'Rock Paper Scissors' },
-    { path: '/DashLanding', name: 'Simple Dashboard Landing' },
-    { path: '/Restaurant', name: 'Restaurant Landing Pages' },
-    { path: '/OdinRecipes', name: 'Recipes Pages' },
-    { path: '/Landing', name: 'First Odin Landing' },
-    { path: '/WeatherApp', name: 'Weather App' },
-  ];
+const projects: Project[] = [
+  { path: '/ToDoApp', name: 'To Do' },
+  { path: '/TicTacToe', name: 'Tic Tac Toe' },
+  { path: '/BookLibrary', name: 'Book Library' },
+  { path: '/SignUpForm', name: 'Sign Up Form' },
+  { path: '/Calculator', name: 'Calculator' },
+  { path: '/EtchASketch', name: 'Etch a Sketch' },
+  { path: '/DrumKit', name: 'Drum Kit' },
+  { path: '/RockPaperScissors', name: 'Rock Paper Scissors' },
+  { path: '/DashLanding', name: 'Simple Dashboard Landing' },
+  { path: '/Restaurant', name: 'Restaurant Landing Pages' },
+  { path: '/OdinRecipes', name: 'Recipes Pages' },
+  { path: '/Landing', name: 'First Odin Landing' },
+  { path: '/WeatherApp', name: 'Weather App' },
+];
 
+const DashboardComponent: React.FC = React.memo(() => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
@@ -74,6 +73,8 @@ const Dashboard = () => {
       </div>
     </ThemeProvider>
   );
-};
+});
 
-export default Dashboard;
+DashboardComponent.displayName = 'Dashboard';
+
+export default React.memo(DashboardComponent);
