cb70c5b7af001d69eabe1bf041e7b2a61f209dad
itkrivoshei <nikitakrivoshei@gmail.com>
2023-12-23 17:41:53 +0100

    Refactor import path in store.ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import { thunk } from 'redux-thunk';
-import rootReducer from './reducers/rootReducer';
+import rootReducer from './rootReducer';
 import { initialState } from './reducers/toDoReducer';
 
 type StateType = {
