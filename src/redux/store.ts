1a55617de2aa37514831e24f1407b3710abcda1b
itkrivoshei <nikitakrivoshei@gmail.com>
2023-12-24 14:59:09 +0100

    Refactor Redux store configuration

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,53 +1,25 @@
-import { createStore, applyMiddleware } from 'redux';
-import { thunk } from 'redux-thunk';
-import rootReducer from './rootReducer';
-import { initialState } from './reducers/toDoReducer';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-type StateType = {
-  toDos?: {
-    projects?: any[];
-  };
-};
+import toDoReducer from './slices/toDoSlice';
+import weatherReducer from './slices/weatherSlice';
 
-// Check if the loaded state is valid
-function isValidState(state: StateType): boolean {
-  return Boolean(state && state.toDos && Array.isArray(state.toDos.projects));
-}
-
-// Save the state to local storage
-function saveState(state: StateType): void {
-  try {
-    const serializedState = JSON.stringify(state);
-    localStorage.setItem('myState', serializedState);
-  } catch (err) {
-    console.error('Could not save state', err);
-  }
-}
-
-// Load the state from local storage
-function loadState() {
-  try {
-    const serializedState = localStorage.getItem('myState');
-    if (serializedState === null) return { toDos: initialState };
-
-    const parsedState = JSON.parse(serializedState);
-
-    return isValidState(parsedState) ? parsedState : { toDos: initialState }; // Validate the loaded state
-  } catch (err) {
-    console.error('Could not load state', err);
-    return { toDos: initialState };
-  }
-}
-
-// Load the persisted state
-const persistedState = loadState();
-
-// Create the Redux store
-const store = createStore(rootReducer, persistedState, applyMiddleware(thunk));
-
-// Subscribe to store changes
-store.subscribe(() => {
-  saveState(store.getState());
+const rootReducer = combineReducers({
+  toDo: toDoReducer,
+  weather: weatherReducer,
 });
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
 export default store;
