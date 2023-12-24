45be4269e3bdb03b71b97f6761c103a7cf4ddd35
itkrivoshei <nikitakrivoshei@gmail.com>
2023-12-24 19:01:34 +0100

    Refactor MainWeatherDisplay component and update weather fetching logic

--- src/components/WeatherApp/MainWeatherDisplay.tsx
+++ src/components/WeatherApp/MainWeatherDisplay.tsx
@@ -1,44 +1,34 @@
+import { useDispatch, useSelector } from 'react-redux';
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector, connect } from 'react-redux';
-import WeatherMeme from './WeatherMeme';
-import {
-  fetchWeatherByLocation,
-  resetWeatherError,
-} from '../../redux/actions/weatherActions';
-import { RootState } from '../../redux/reducers/rootReducer';
+
+import { fetchWeather } from '../../redux/slices/weatherSlice';
+import { RootState, AppDispatch } from '../../redux/store';
 import './styles/MainWeatherDisplay.scss';
+import WeatherMeme from './WeatherMeme';
 
-// Utility function for displaying temperature
-const displayTemperature = (temp: number, unit: string) => `${temp} ${unit}`;
-
-// Utility function for displaying wind speed
-const displayWindSpeed = (speed: number, unit: string) => `${speed} ${unit}`;
-
-// Main Weather Display Component
-export const MainWeatherDisplay: React.FC = () => {
-  // useDispatch hook to dispatch actions
-  const dispatch = useDispatch();
-
-  // useSelector hook to select data from the redux store
-  const { weatherData, error, unit } = useSelector(
+const MainWeatherDisplay: React.FC = () => {
+  const dispatch: AppDispatch = useDispatch();
+  const { weatherData, error, region } = useSelector(
     (state: RootState) => state.weather
   );
 
-  // Similar to componentDidMount and componentDidUpdate:
   useEffect(() => {
-    // Fetch weather data by location on component mount
-    dispatch(fetchWeatherByLocation() as any);
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          dispatch(fetchWeather({ latitude, longitude }));
+        },
+        (error) => {
+          console.error('Geolocation error:', error.message);
+        }
+      );
+    } else {
+      console.error('Geolocation is not supported by this browser.');
+    }
+  }, [dispatch]);
 
-    // Reset weather error when component unmounts
-    return () => {
-      dispatch(resetWeatherError());
-    };
-  }, [dispatch]); // Empty array ensures effect only runs on mount and unmount
-
-  // Handle error state
   if (error) {
-    console.error('Rendering error:', error);
-    console.error('Weather data when error:', weatherData);
     return (
       <div className='main-weather-display'>
         <div className='error'>Error loading weather: {error}</div>
@@ -46,105 +36,59 @@ export const MainWeatherDisplay: React.FC = () => {
     );
   }
 
-  const formatDateAndTime = (dateString: string) => {
-    const options: Intl.DateTimeFormatOptions = {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit',
-      hour12: false,
-    };
-    return new Date(dateString).toLocaleString(undefined, options);
-  };
+  if (!weatherData) {
+    return <div>Loading...</div>;
+  }
 
-  // Component JSX
+  const displayTemperature = (temp: number) => `${temp}°${region}`;
+  const displayWindSpeed = (speed: number) =>
+    `${speed} ${region === 'EU' ? 'kph' : 'mph'}`;
+
   return (
     <div className='main-weather-display'>
-      {weatherData && (
-        <>
-          <div className='location'>
-            {weatherData.location.name}, {weatherData.location.country}
+      <div className='location'>
+        {weatherData.location.name}, {weatherData.location.country}
+      </div>
+      <div className='date'>
+        {new Date(weatherData.location.localtime).toLocaleString()}
+      </div>
+
+      {/* Main weather info */}
+      <div className='main-info-container'>
+        {/* Temperature Section */}
+        <div className='temperature-section'>
+          <div className='temperature'>
+            {/* {displayTemperature(weatherData.current)} */}
           </div>
-          <div className='date'>
-            {formatDateAndTime(weatherData.location.localtime)}
+          <div className='feels'>
+            Feels: {displayTemperature(weatherData.current.feelslike_c)}
           </div>
+        </div>
 
-          <div className='main-info-container'>
-            <div className='temperature-section'>
-              <div className='temperature'>
-                {displayTemperature(
-                  unit === 'C'
-                    ? weatherData.current.feelslike_c
-                    : weatherData.current.feelslike_f,
-                  unit
-                )}
-                <span className='degree-symbol'>°{unit}</span>
-              </div>
-              <div className='feels'>
-                Feels:{' '}
-                {displayTemperature(
-                  unit === 'C'
-                    ? weatherData.current.feelslike_c
-                    : weatherData.current.feelslike_f,
-                  unit
-                )}
-                <span className='degree-symbol'>°{unit}</span>
-              </div>
-            </div>
+        <div className='divider' />
 
-            <div className='divider' />
-
-            <div className='details'>
-              <div className='detail'>
-                Wind:{' '}
-                {displayWindSpeed(
-                  unit === 'C'
-                    ? weatherData.current.wind_kph
-                    : weatherData.current.wind_mph,
-                  unit === 'C' ? 'kph' : 'mph'
-                )}
-              </div>
-              <div className='detail'>
-                Humidity: {weatherData.current.humidity}%
-              </div>
-              <div className='detail'>
-                Air Quality EPA:{' '}
-                {weatherData.current.air_quality?.['us-epa-index'] ||
-                  'Not available'}
-              </div>
-            </div>
-
-            <div className='divider' />
-
-            <WeatherMemeSection
-              weatherCondition={weatherData.current.condition.text}
-            />
+        {/* Other Details */}
+        <div className='details'>
+          <div className='detail'>
+            Wind: {displayWindSpeed(weatherData.current.wind_kph)}
           </div>
-        </>
-      )}
+          <div className='detail'>
+            Humidity: {weatherData.current.humidity}%
+          </div>
+          <div className='detail'>
+            Air Quality:{' '}
+            {/* {weatherData.current.air_quality['us-epa-index'] || 'N/A'} */}
+          </div>
+        </div>
+
+        <div className='divider' />
+
+        {/* Weather Meme Section */}
+        <WeatherMeme />
+        <div>Condition: {weatherData.current.condition.text}</div>
+      </div>
     </div>
   );
 };
 
-// Separate component for the weather meme section
-const WeatherMemeSection: React.FC<{ weatherCondition: string }> = ({
-  weatherCondition,
-}) => (
-  <div className='weather-meme-section'>
-    <WeatherMeme />
-    <div>Weather: {weatherCondition}</div>
-  </div>
-);
-
-// Connect the component to the redux store
-const mapStateToProps = (state: RootState) => ({
-  weatherData: state.weather.weatherData,
-  isLoading: state.weather.loading,
-  error: state.weather.error,
-  unit: state.weather.unit,
-});
-
-// Use named export for better import clarity
-export const ConnectedMainWeatherDisplay =
-  connect(mapStateToProps)(MainWeatherDisplay);
+export default MainWeatherDisplay;
