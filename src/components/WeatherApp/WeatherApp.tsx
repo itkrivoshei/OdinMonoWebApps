34cd460f9803dd88a6307e1fde22b8ffff1f1ea7
itkrivoshei <nikitakrivoshei@gmail.com>
2024-01-05 22:00:00 +0100

    Add dark theme to WeatherApp

--- src/components/WeatherApp/WeatherApp.tsx
+++ src/components/WeatherApp/WeatherApp.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { ThemeProvider, createTheme } from '@mui/material';
 
 import MainWeatherDisplay from './MainWeatherDisplay';
 import SettingsMenu from './SettingsMenu';
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 const WeatherApp = () => {
   return (
-    <div>
+    <ThemeProvider theme={darkTheme}>
       <SettingsMenu />
       <MainWeatherDisplay />
-    </div>
+    </ThemeProvider>
   );
 };
 
