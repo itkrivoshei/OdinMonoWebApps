a0fc3c8d05a5eca1ead33fdcb2a9a1c3e99ce476
itkrivoshei <nikitakrivoshei@gmail.com>
2024-01-07 19:47:40 +0100

    Refactor WeatherApp component with MUI theme

--- src/components/WeatherApp/WeatherApp.tsx
+++ src/components/WeatherApp/WeatherApp.tsx
@@ -1,28 +1,36 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import { Box, createTheme, ThemeProvider } from '@mui/material';
 
 import cosmosImage from '../../assets/WeatherApp/images/cosmos_image.jpg';
 import MainWeatherDisplay from './MainWeatherDisplay';
 import SettingsMenu from './SettingsMenu';
 
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Orbitron, sans-serif',
+  },
+});
+
 const WeatherApp = () => {
   return (
-    <Box
-      sx={{
-        backgroundImage: `url(${cosmosImage})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        width: '100vw',
-        height: '100vh',
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        zIndex: -1,
-      }}
-    >
-      <SettingsMenu />
-      <MainWeatherDisplay />
-    </Box>
+    <ThemeProvider theme={theme}>
+      <Box
+        sx={{
+          backgroundImage: `url(${cosmosImage})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          width: '100vw',
+          height: '100vh',
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          zIndex: -1,
+        }}
+      >
+        <SettingsMenu />
+        <MainWeatherDisplay />
+      </Box>
+    </ThemeProvider>
   );
 };
 
