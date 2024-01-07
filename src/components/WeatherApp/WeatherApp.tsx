a0f8d12b9fe876911237a93070691069f52a0160
itkrivoshei <nikitakrivoshei@gmail.com>
2024-01-07 13:19:02 +0100

    Add background image to WeatherApp component

--- src/components/WeatherApp/WeatherApp.tsx
+++ src/components/WeatherApp/WeatherApp.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import { Box } from '@mui/material';
 
+import cosmosImage from '../../assets/WeatherApp/images/cosmos_image.jpg';
 import MainWeatherDisplay from './MainWeatherDisplay';
 import SettingsMenu from './SettingsMenu';
 
 const WeatherApp = () => {
   return (
-    <>
+    <Box
+      sx={{
+        backgroundImage: `url(${cosmosImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        width: '100vw',
+        height: '100vh',
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        zIndex: -1,
+      }}
+    >
       <SettingsMenu />
       <MainWeatherDisplay />
-    </>
+    </Box>
   );
 };
 
