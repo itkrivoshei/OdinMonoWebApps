385291526d34d3a35e9caf704e15d77823e4f363
itkrivoshei <nikitakrivoshei@gmail.com>
2024-01-08 14:09:14 +0100

    Add MUI theme and background image to ToDoApp

--- src/components/ToDoApp/ToDoApp.tsx
+++ src/components/ToDoApp/ToDoApp.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Box, Typography } from '@mui/material';
+import {
+  Box,
+  Typography,
+  ThemeProvider,
+  createTheme,
+  Paper,
+} from '@mui/material';
 
 import { RootState } from '../../redux/store';
 
@@ -8,6 +14,7 @@ import AddToDo from './AddToDo';
 import ToDoList from './ToDoList';
 import ProjectList from './ProjectList';
 import AddProject from './AddProject';
+import lofiImage from '../../assets/ToDoApp/images/lofi_image.jpg';
 
 const ToDoApp: React.FC = () => {
   const activeProject = useSelector((state: RootState) =>
@@ -16,16 +23,84 @@ const ToDoApp: React.FC = () => {
     )
   );
 
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#7161ef',
+      },
+      secondary: {
+        main: '#cdb4dbff',
+      },
+    },
+    typography: {
+      fontFamily: '"Space Mono", monospace',
+    },
+  });
+
   return (
-    <Box className='toDoApp' sx={{ padding: 2 }}>
-      <Typography variant='h1'>ToDo App</Typography>
-      <Typography variant='h2'>Projects</Typography>
-      <AddProject />
-      <ProjectList />
-      <Typography variant='h2'>ToDo&apos;s</Typography>
-      <AddToDo />
-      {activeProject && <ToDoList toDos={activeProject.toDos} />}
-    </Box>
+    <ThemeProvider theme={theme}>
+      <Box
+        sx={{
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          display: 'flex',
+          width: '100vw',
+          height: '100vh',
+          position: 'fixed',
+          zIndex: -1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundImage: `url(${lofiImage})`,
+        }}
+      >
+        <Paper
+          elevation={3}
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            maxWidth: '90vw',
+            maxHeight: '90vh',
+            backgroundColor: '#9381ff',
+            width: '70vh',
+            height: '50vh',
+            opacity: 0.9,
+            padding: 2,
+          }}
+        >
+          <Box
+            sx={{
+              width: '35%',
+              backgroundColor: 'secondary.main',
+              padding: 2,
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'start',
+              alignItems: 'center',
+            }}
+          >
+            <Typography variant='h4' sx={{ m3: 2 }}>
+              ToDoApp
+            </Typography>
+            <Typography variant='h6'>Projects</Typography>
+            <AddProject />
+            <ProjectList />
+          </Box>
+          <Box
+            sx={{
+              width: '65%',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+              paddingLeft: 2,
+            }}
+          >
+            <AddToDo />
+            {activeProject && <ToDoList toDos={activeProject.toDos} />}
+          </Box>
+        </Paper>
+      </Box>
+    </ThemeProvider>
   );
 };
 
