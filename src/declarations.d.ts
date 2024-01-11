6156abdada4574171a3f0b823510b984556e0973
itkrivoshei <nikitakrivoshei@gmail.com>
2024-01-11 16:47:48 +0100

    Add declarations for mp3 and svg files

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -7,3 +7,17 @@ declare module '*.jpg' {
   const value: string;
   export default value;
 }
+
+declare module '*.mp3' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.svg' {
+  import React = require('react');
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement>
+  >;
+  const src: string;
+  export default src;
+}
