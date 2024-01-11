8c8bbd4ee0cc3d7c1a9dea0f050cf91fd183b54e
itkrivoshei <nikitakrivoshei@gmail.com>
2024-01-11 16:47:52 +0100

    Update Button component to use SVG component

--- src/components/Calculator/Button.tsx
+++ src/components/Calculator/Button.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 interface ButtonProps {
   id: string;
   dataValue: string;
-  imageSrc: string;
+  SvgComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>; // Accept a React component type for SVGs
   onMouseDown: (value: string) => void;
   onMouseUp: (value: string) => void;
   isLongKey?: boolean;
@@ -13,7 +13,7 @@ interface ButtonProps {
 const Button: React.FC<ButtonProps> = ({
   id,
   dataValue,
-  imageSrc,
+  SvgComponent,
   onMouseDown,
   onMouseUp,
   isLongKey,
@@ -27,8 +27,9 @@ const Button: React.FC<ButtonProps> = ({
       }`}
       onMouseDown={() => onMouseDown(dataValue)}
       onMouseUp={() => onMouseUp(dataValue)}
+      aria-label={dataValue}
     >
-      <img src={imageSrc} alt={dataValue} />
+      <SvgComponent className='button-svg' />
     </button>
   );
 };
